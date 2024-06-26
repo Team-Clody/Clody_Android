d3b626fded8ac075cf586c9eba1b12b16c6bd8e0
Moonsu Kang <rkdms4971@naver.com>
2024-06-26 16:38:40 +0900

    [Setting/#1] MainActivity

--- app/src/main/java/com/sopt/clody/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/clody/presentation/MainActivity.kt
@@ -1,18 +1,21 @@
-package com.sopt.clody
+package com.sopt.clody.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.sopt.clody.ui.theme.CLODYTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val exampleViewModel: ExampleViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ExampleScreen(exampleViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CLODYTheme {
-        Greeting("Android")
     }
 }
