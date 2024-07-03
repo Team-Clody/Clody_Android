1f58369dae76b22b3fd620228f105cf7e00a6539
SYAAINN <alswo08319@gmail.com>
2024-07-03 11:37:32 +0900

    [Setting/#3] color, font provider 생성

--- app/src/main/java/com/sopt/clody/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/clody/ui/theme/Theme.kt
@@ -1,70 +1,70 @@
 package com.sopt.clody.ui.theme
 
 import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+object ClodyTheme {
+    val colors: ClodyColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalClodyColors.current
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+    val typography: ClodyTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
+}
 
 @Composable
-fun CLODYTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+fun provideClodyColorsAndTypography(
+    colors: ClodyColors,
+    typography: ClodyTypography,
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val provideColors = remember { colors.copy() }
+    val provideTypography = remember { typography.copy() }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+    CompositionLocalProvider(
+        LocalClodyColors provides provideColors,
+        LocalTypography provides provideTypography,
+        content = content,
     )
+}
+
+@Composable
+fun CLODYTheme(content: @Composable () -> Unit) {
+    val color = defaultClodyColors
+    val typography = defaultClodyTypography
+
+    provideClodyColorsAndTypography(color, typography) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            // SideEffect를 사용하여 상태 표시줄의 색상을 설정
+            SideEffect {
+                val window = (view.context as Activity).window
+                window.statusBarColor = Color.White.toArgb() // 상태 표시줄 색상을 흰색으로 설정
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true // 상태 표시줄 아이콘 색상을 항상 검정색으로 설정
+            }
+        }
+
+        // Surface를 사용하여 전체 배경색을 설정
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.White
+        ) {
+            MaterialTheme(content = content)
+        }
+    }
 }
