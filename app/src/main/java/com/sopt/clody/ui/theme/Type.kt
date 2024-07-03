6dfcece622ca0f86b9fcfe921424a908bc7f0061
SYAAINN <alswo08319@gmail.com>
2024-07-03 11:35:13 +0900

    [Setting/#3] Font setting

--- app/src/main/java/com/sopt/clody/ui/theme/Type.kt
+++ app/src/main/java/com/sopt/clody/ui/theme/Type.kt
@@ -1,34 +1,129 @@
 package com.sopt.clody.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sopt.clody.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+val pretendardFontFamily = FontFamily(
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal)
+)
+
+private val pretendardTextStyle = TextStyle(
+    fontFamily = pretendardFontFamily,
+    fontWeight = FontWeight.SemiBold,
+    letterSpacing = (-0.2).sp,
+)
+
+val defaultClodyTypography = ClodyTypography(
+    head1 = pretendardTextStyle.copy(
+        fontSize = 22.sp,
+        lineHeight = 33.sp,
+    ),
+    head2 = pretendardTextStyle.copy(
+        fontSize = 20.sp,
+        lineHeight = 30.sp,
+    ),
+    head3 = pretendardTextStyle.copy(
+        fontSize = 18.sp,
+        lineHeight = 27.sp,
+    ),
+    head4 = pretendardTextStyle.copy(
+        fontSize = 17.sp,
+        lineHeight = 25.5.sp,
+    ),
+    head5 = pretendardTextStyle.copy(
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    body1SemiBold = pretendardTextStyle.copy(
+        fontSize = 15.sp,
+        lineHeight = 22.5.sp,
+    ),
+    body1Medium = pretendardTextStyle.copy(
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 15.sp,
+        lineHeight = 22.5.sp,
+    ),
+    body2SemiBold = pretendardTextStyle.copy(
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+    ),
+    body2Medium = pretendardTextStyle.copy(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+    ),
+    body3SemiBold = pretendardTextStyle.copy(
+        fontSize = 13.sp,
+        lineHeight = 19.5.sp,
+    ),
+    body3Medium = pretendardTextStyle.copy(
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+        lineHeight = 19.5.sp,
+    ),
+    detail1SemiBold = pretendardTextStyle.copy(
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+    ),
+    detail1Medium = pretendardTextStyle.copy(
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+    ),
+    detail2SemiBold = pretendardTextStyle.copy(
+        fontSize = 10.sp,
+        lineHeight = 15.sp,
+    ),
+    detail2Medium = pretendardTextStyle.copy(
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
+        lineHeight = 15.sp,
     )
-    */
 )
+
+@Immutable
+data class ClodyTypography(
+    val head1: TextStyle,
+    val head2: TextStyle,
+    val head3: TextStyle,
+    val head4: TextStyle,
+    val head5: TextStyle,
+    val body1SemiBold: TextStyle,
+    val body1Medium: TextStyle,
+    val body2SemiBold: TextStyle,
+    val body2Medium: TextStyle,
+    val body3SemiBold: TextStyle,
+    val body3Medium: TextStyle,
+    val detail1SemiBold: TextStyle,
+    val detail1Medium: TextStyle,
+    val detail2SemiBold: TextStyle,
+    val detail2Medium: TextStyle
+)
+
+val LocalTypography = staticCompositionLocalOf {
+    ClodyTypography(
+        head1 = pretendardTextStyle,
+        head2 = pretendardTextStyle,
+        head3 = pretendardTextStyle,
+        head4 = pretendardTextStyle,
+        head5 = pretendardTextStyle,
+        body1SemiBold = pretendardTextStyle,
+        body1Medium = pretendardTextStyle,
+        body2SemiBold = pretendardTextStyle,
+        body2Medium = pretendardTextStyle,
+        body3SemiBold = pretendardTextStyle,
+        body3Medium = pretendardTextStyle,
+        detail1SemiBold = pretendardTextStyle,
+        detail1Medium = pretendardTextStyle,
+        detail2SemiBold = pretendardTextStyle,
+        detail2Medium = pretendardTextStyle
+    )
+}
