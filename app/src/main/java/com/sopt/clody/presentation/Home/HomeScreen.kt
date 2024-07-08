a638b564886a887b6bcbed6ca50a65cc2d0f916d
yeonjeen <jeendev2019@gmail.com>
2024-07-08 23:11:05 +0900

    [FEAT/#9] 클러버 카운트 추가

--- app/src/main/java/com/sopt/clody/presentation/Home/HomeScreen.kt
+++ app/src/main/java/com/sopt/clody/presentation/Home/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.sopt.clody.presentation.Home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,16 +11,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.clody.R
+import com.sopt.clody.ui.theme.ClodyTheme
 
 @Composable
 fun HomeScreen() {
@@ -31,6 +36,8 @@ fun HomeScreen() {
             .verticalScroll(scrollState)
     ) {
         HomeHeader()
+        CloverCount()
+        ClodyCalendar()
     }
 }
 
@@ -39,36 +46,58 @@ fun HomeHeader() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(end = 20.dp),
+                .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_home_list),
                 contentDescription = "go to list",
-                modifier = Modifier.size(24.dp)
+                modifier = Modifier
             )
 
             Spacer(modifier = Modifier.weight(1f))
-
             //TODO 월 설정 스피너 들어갈 예정
 
             Image(
                 painter = painterResource(id = R.drawable.ic_home_setting),
                 contentDescription = "go to setting",
                 modifier = Modifier
-                    .size(24.dp)
-                    .padding(end = 20.dp)
             )
         }
     }
 }
 
+@Composable
+fun CloverCount(
+) {
+    val text = "클러버 23개"
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 22.dp)
+    ) {
+        Text(
+            text = text,
+            style = ClodyTheme.typography.detail1SemiBold,
+            color = ClodyTheme.colors.darkGreen,
+            modifier = Modifier
+                .border(9.dp, ClodyTheme.colors.lightGreen, shape = RoundedCornerShape(4.dp))
+                .background(ClodyTheme.colors.lightGreen, shape = RoundedCornerShape(4.dp))
+                .padding(12.dp),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun ClodyCalendar() {
+
+}
 
 @Preview(showBackground = true)
 @Composable
