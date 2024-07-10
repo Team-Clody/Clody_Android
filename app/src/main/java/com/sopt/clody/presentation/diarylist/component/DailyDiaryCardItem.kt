c60dda1a629efb78af2fa091ab3fa2b6c213e14a
SYAAINN <alswo08319@gmail.com>
2024-07-10 17:01:57 +0900

    [UI/#8] 해당하는 일자에 작성한 일기 리스트 구현

---
+++ app/src/main/java/com/sopt/clody/presentation/diarylist/component/DailyDiaryCardItem.kt
@@ -0,0 +1,28 @@
+package com.sopt.clody.presentation.diarylist.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sopt.clody.ui.theme.ClodyTheme
+
+@Composable
+fun DailyDiaryCardItem(index: Int) {
+    val diaryList = (1..(index % 5 + 1)).map { i -> "마지막 세미나에 참석할 수 있어 감사해,마지막 세미나에 참석할 수 있어 감사해0000000$i" }
+
+    Column {
+        diaryList.forEachIndexed { idx, entry ->
+            Row(modifier = Modifier.padding(top = 18.dp)) {
+                Text(text = "${idx + 1}.", style = ClodyTheme.typography.body2SemiBold)
+                Text(
+                    text = entry,
+                    modifier = Modifier.padding(start = 10.dp),
+                    style = ClodyTheme.typography.body2SemiBold
+                )
+            }
+        }
+    }
+}
