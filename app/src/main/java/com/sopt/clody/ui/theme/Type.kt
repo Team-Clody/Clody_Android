634abf9f08876660cae2e9ada8029866d4b2ace9
SYAAINN <alswo08319@gmail.com>
2024-07-10 18:42:49 +0900

    [SETTING/#15] font asset 추가

--- app/src/main/java/com/sopt/clody/ui/theme/Type.kt
+++ app/src/main/java/com/sopt/clody/ui/theme/Type.kt
@@ -12,81 +12,116 @@ import com.sopt.clody.R
 
 val pretendardFontFamily = FontFamily(
     Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
     Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal)
 )
 
 private val pretendardTextStyle = TextStyle(
     fontFamily = pretendardFontFamily,
-    fontWeight = FontWeight.SemiBold,
     letterSpacing = (-0.2).sp,
 )
 
 val defaultClodyTypography = ClodyTypography(
     head1 = pretendardTextStyle.copy(
         fontSize = 22.sp,
+        fontWeight = FontWeight.SemiBold,
         lineHeight = 33.sp,
     ),
     head2 = pretendardTextStyle.copy(
         fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
         lineHeight = 30.sp,
     ),
     head3 = pretendardTextStyle.copy(
         fontSize = 18.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 27.sp,
+    ),
+    head3Medium = pretendardTextStyle.copy(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Medium,
         lineHeight = 27.sp,
     ),
     head4 = pretendardTextStyle.copy(
         fontSize = 17.sp,
+        fontWeight = FontWeight.SemiBold,
         lineHeight = 25.5.sp,
     ),
-    head5 = pretendardTextStyle.copy(
+    body1SemiBold = pretendardTextStyle.copy(
         fontSize = 16.sp,
+        fontWeight = FontWeight.SemiBold,
         lineHeight = 24.sp,
     ),
-    body1SemiBold = pretendardTextStyle.copy(
-        fontSize = 15.sp,
-        lineHeight = 22.5.sp,
-    ),
     body1Medium = pretendardTextStyle.copy(
+        fontSize = 16.sp,
         fontWeight = FontWeight.Medium,
-        fontSize = 15.sp,
-        lineHeight = 22.5.sp,
+        lineHeight = 24.sp,
     ),
     body2SemiBold = pretendardTextStyle.copy(
-        fontSize = 14.sp,
-        lineHeight = 21.sp,
+        fontSize = 15.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 22.5.sp,
     ),
     body2Medium = pretendardTextStyle.copy(
+        fontSize = 15.sp,
         fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
-        lineHeight = 21.sp,
+        lineHeight = 22.5.sp,
     ),
     body3SemiBold = pretendardTextStyle.copy(
-        fontSize = 13.sp,
-        lineHeight = 19.5.sp,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 21.sp,
     ),
     body3Medium = pretendardTextStyle.copy(
+        fontSize = 14.sp,
         fontWeight = FontWeight.Medium,
+        lineHeight = 21.sp,
+    ),
+    body3Regular = pretendardTextStyle.copy(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 21.sp,
+    ),
+    body4Medium = pretendardTextStyle.copy(
         fontSize = 13.sp,
-        lineHeight = 19.5.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 19.5.sp
+    ),
+    body4SemiBold = pretendardTextStyle.copy(
+        fontSize = 13.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 19.5.sp
     ),
     detail1SemiBold = pretendardTextStyle.copy(
         fontSize = 12.sp,
+        fontWeight = FontWeight.SemiBold,
         lineHeight = 18.sp,
     ),
     detail1Medium = pretendardTextStyle.copy(
-        fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
+        fontWeight = FontWeight.Medium,
         lineHeight = 18.sp,
+    ),
+    detail1Regular = pretendardTextStyle.copy(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 18.sp
     ),
     detail2SemiBold = pretendardTextStyle.copy(
         fontSize = 10.sp,
+        fontWeight = FontWeight.SemiBold,
         lineHeight = 15.sp,
     ),
     detail2Medium = pretendardTextStyle.copy(
-        fontWeight = FontWeight.Medium,
         fontSize = 10.sp,
+        fontWeight = FontWeight.Medium,
         lineHeight = 15.sp,
-    )
+    ),
+    letterMedium = pretendardTextStyle.copy(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 26.6.sp
+    ),
 )
 
 @Immutable
@@ -94,18 +129,23 @@ data class ClodyTypography(
     val head1: TextStyle,
     val head2: TextStyle,
     val head3: TextStyle,
+    val head3Medium: TextStyle,
     val head4: TextStyle,
-    val head5: TextStyle,
     val body1SemiBold: TextStyle,
     val body1Medium: TextStyle,
     val body2SemiBold: TextStyle,
     val body2Medium: TextStyle,
     val body3SemiBold: TextStyle,
     val body3Medium: TextStyle,
+    val body3Regular: TextStyle,
+    val body4SemiBold: TextStyle,
+    val body4Medium: TextStyle,
     val detail1SemiBold: TextStyle,
     val detail1Medium: TextStyle,
+    val detail1Regular: TextStyle,
     val detail2SemiBold: TextStyle,
-    val detail2Medium: TextStyle
+    val detail2Medium: TextStyle,
+    val letterMedium: TextStyle,
 )
 
 val LocalTypography = staticCompositionLocalOf {
