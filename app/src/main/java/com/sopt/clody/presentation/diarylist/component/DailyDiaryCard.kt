295e35dbb0b91bf87cb157c85af94952331b7979
SYAAINN <alswo08319@gmail.com>
2024-07-10 17:03:34 +0900

    [UI/#8] 일별 일기 리스트를 담은 카드 아이템 구현

---
+++ app/src/main/java/com/sopt/clody/presentation/diarylist/component/DailyDiaryCard.kt
@@ -0,0 +1,93 @@
+package com.sopt.clody.presentation.diarylist.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.sopt.clody.R
+import com.sopt.clody.ui.theme.ClodyTheme
+
+@Composable
+fun DailyDiaryCard(index: Int, order: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp)
+            .padding(bottom = 14.dp)
+            .wrapContentSize(align = Alignment.TopStart),
+        colors = CardDefaults.cardColors(containerColor = ClodyTheme.colors.white)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(28.dp),
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_listview_clover),
+                    contentDescription = "clover",
+                    modifier = Modifier
+                        .padding(end = 6.dp)
+                )
+                Text(
+                    text = "26일",
+                    modifier = Modifier
+                        .padding(end = 2.dp),
+                    style = ClodyTheme.typography.body1SemiBold
+                )
+                Text(
+                    text = "/목요일",
+                    style = ClodyTheme.typography.body2SemiBold,
+                    color = ClodyTheme.colors.gray04
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    colors = ButtonDefaults.buttonColors(containerColor = ClodyTheme.colors.lightBlue),
+                    shape = RoundedCornerShape(size = 9.dp),
+                    onClick = { /* 답장 확인 */ },
+                    contentPadding = PaddingValues(0.dp) // button에 설정된 기본 패딩을 제거
+                ) {
+                    Text(
+                        text = "답장 확인",
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp, vertical = 2.dp),
+                        style = ClodyTheme.typography.detail1SemiBold,
+                        color = ClodyTheme.colors.blue
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.ic_listview_kebab_menu),
+                    contentDescription = "케밥 메뉴",
+                    modifier = Modifier
+                        .padding(start = 14.dp)
+                        .clickable(onClick = {})
+                )
+            }
+            DailyDiaryCardItem(index)
+        }
+    }
+}
