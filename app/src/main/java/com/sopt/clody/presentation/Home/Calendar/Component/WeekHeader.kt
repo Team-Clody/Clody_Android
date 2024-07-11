91017452d0f2cdb00bd6ec6369c38204ff98da05
yeonjeen <jeendev2019@gmail.com>
2024-07-12 00:54:49 +0900

    [CHORE/#9] 요일헤더 넓이 조정

--- app/src/main/java/com/sopt/clody/presentation/Home/Calendar/Component/WeekHeader.kt
+++ app/src/main/java/com/sopt/clody/presentation/Home/Calendar/Component/WeekHeader.kt
@@ -1,25 +1,29 @@
 package com.sopt.clody.presentation.Home.Calendar.Component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.sopt.clody.ui.theme.ClodyTheme
 import kotlinx.datetime.DayOfWeek
 
 @Composable
-fun WeekHeader(modifier: Modifier = Modifier) {
+fun WeekHeader(modifier: Modifier = Modifier, itemWidth: Dp) {
     val itemWidth = (LocalConfiguration.current.screenWidthDp.dp - 40.dp) / 7
     Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = modifier.padding(top = 16.dp, bottom = 7.dp)
+        horizontalArrangement = Arrangement.SpaceBetween, // 고르게 분포되도록 설정
+        modifier = modifier.fillMaxWidth()
     ) {
         val weekLabelArray = listOf(
             DayOfWeek.SUNDAY,
@@ -34,12 +38,16 @@ fun WeekHeader(modifier: Modifier = Modifier) {
         val koreanWeekLabels = weekLabelArray.map { it.toKoreanShortLabel() }
 
         koreanWeekLabels.forEach { week ->
-            Text(
-                text = week,
-                style = ClodyTheme.typography.body3SemiBold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.width(itemWidth)
-            )
+            Box(
+                modifier = Modifier.width(itemWidth),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = week,
+                    style = ClodyTheme.typography.body3SemiBold,
+                    textAlign = TextAlign.Center,
+                )
+            }
         }
     }
 }
@@ -59,5 +67,4 @@ fun DayOfWeek.toKoreanShortLabel(): String {
 @Composable
 @Preview(showBackground = true)
 fun WeekHeaderPreview() {
-    WeekHeader()
 }
