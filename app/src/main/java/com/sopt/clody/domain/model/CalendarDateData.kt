c3174d50b7a0e0391fe472263b9b155cbc9201af
yeonjeen <jeendev2019@gmail.com>
2024-07-15 02:08:20 +0900

    [CHORE/#9] 캘린더 날짜 데이터클래스, 일기 데이터클래스 분리

--- app/src/main/java/com/sopt/clody/domain/model/CalendarDateData.kt
+++ app/src/main/java/com/sopt/clody/domain/model/CalendarDateData.kt
@@ -1,6 +1,5 @@
 package com.sopt.clody.domain.model
 
-import java.time.LocalDate
 import java.time.YearMonth
 
 data class CalendarDate(
@@ -9,17 +8,11 @@ data class CalendarDate(
     val year: Int
 )
 
-data class DiaryData(
-    val diaryCount: Int,
-    val replyStatus: String,
-)
-
 fun daysInMonth(month: Int, year: Int): Int {
     return YearMonth.of(year, month).lengthOfMonth()
 }
 
 fun generateCalendarDates(year: Int, month: Int): List<CalendarDate> {
-    val firstDayOfMonth = LocalDate.of(year, month, 1)
     val daysInCurrentMonth = daysInMonth(month, year)
 
     return (1..daysInCurrentMonth).map { day ->
