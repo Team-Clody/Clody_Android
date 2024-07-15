3562d4444d5861444b4b499e21b5158f3bc36f1f
SYAAINN <alswo08319@gmail.com>
2024-07-16 06:12:12 +0900

    [FEAT/#43] DiaryList 화면에서 사용할 Service 생성

---
+++ app/src/main/java/com/sopt/clody/data/remote/api/DiaryListService.kt
@@ -0,0 +1,25 @@
+package com.sopt.clody.data.remote.api
+
+import com.sopt.clody.data.remote.dto.base.ApiResponse
+import com.sopt.clody.data.remote.dto.diarylist.MonthlyDiaryData
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface DiaryListService {
+    @GET("api/v1/calender/list")
+    suspend fun getMonthlyDiary(
+        @Header("accessToken") accessToken: String,
+        @Query("year") year: Int,
+        @Query("month") month: Int,
+    ): ApiResponse<MonthlyDiaryData>
+
+    @DELETE("api/v1/calender")
+    suspend fun deleteDailyDiary(
+        @Header("accessToken") accessToken: String,
+        @Query("year") year: Int,
+        @Query("month") month: Int,
+        @Query("date") date: Int,
+    ): ApiResponse<Unit>
+}
