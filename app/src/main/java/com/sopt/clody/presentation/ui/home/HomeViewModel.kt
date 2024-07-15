d4a9e31d757e0b6fb26ce834571c28d1c7d72af6
yeonjeen <jeendev2019@gmail.com>
2024-07-16 00:30:37 +0900

    [FEAT/#40] 홈 뷰모델

---
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/HomeViewModel.kt
@@ -0,0 +1,28 @@
+package com.sopt.clody.presentation.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.clody.data.remote.dto.base.ApiResponse
+import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
+import com.sopt.clody.data.repository.MonthlyCalendarRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: MonthlyCalendarRepository
+) : ViewModel() {
+    private val _calendarData = MutableStateFlow<ApiResponse<List<MonthlyCalendarResponseDto>>?>(null)
+    val monthlyCalendarData: StateFlow<ApiResponse<List<MonthlyCalendarResponseDto>>?> get() = _calendarData
+
+    fun loadCalendarData(year: Int, month: Int) {
+        viewModelScope.launch {
+            val data = repository.getMonthlyCalendarData(year, month)
+            _calendarData.value = data
+        }
+    }
+
+}
