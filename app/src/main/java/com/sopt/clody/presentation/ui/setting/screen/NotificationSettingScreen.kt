30e292b20b09d095411dd956ea0c13bebc40630f
yeonjeen <jeendev2019@gmail.com>
2024-07-15 09:34:22 +0900

    [FEAT/#9] 알림설정뷰 구현

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/NotificationSettingScreen.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/NotificationSettingScreen.kt
@@ -1,25 +1,33 @@
 package com.sopt.clody.presentation.ui.setting.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.clody.R
+import com.sopt.clody.presentation.ui.component.popup.ClodyPopupBottomSheet
+import com.sopt.clody.presentation.ui.setting.component.SettingBottomSheetTimePicker
+import com.sopt.clody.presentation.ui.setting.component.SettingTopAppBar
 import com.sopt.clody.presentation.ui.setting.navigation.SettingNavigator
 import com.sopt.clody.ui.theme.ClodyTheme
 
@@ -34,38 +42,36 @@ fun NotificationSettingRoute(
 
 @Composable
 fun NotificationSettingScreen(onBackClick: () -> Unit) {
-    Column(modifier = Modifier.fillMaxWidth()) {
+    var showBottomSheet by remember { mutableStateOf(false) }
+    var selectedTime by remember { mutableStateOf("오후 9시 30분") }
 
-        Spacer(modifier = Modifier.height(20.dp))
-        NotificationSettingHeader()
-        Spacer(modifier = Modifier.height(40.dp))
-        DiaryWrittingAlarmSetting()
-        Spacer(modifier = Modifier.height(12.dp))
-        AlarmTime()
-        Spacer(modifier = Modifier.height(12.dp))
-        ReplyAlarmSetting()
-
+    Scaffold(
+        topBar = { SettingTopAppBar(stringResource(R.string.setting_notification_setting), onBackClick) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(top = 20.dp)
+        ) {
+            DiaryWrittingAlarmSetting()
+            Spacer(modifier = Modifier.height(32.dp))
+            AlarmTime(
+                showBottomSheetStateChange = { newState -> showBottomSheet = newState}
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            ReplyAlarmSetting()
+        }
     }
-}
-
-@Composable
-fun NotificationSettingHeader() {
-    Row(
-        modifier = Modifier.padding(horizontal = 12.dp),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Image(
-            painterResource(id = R.drawable.ic_setting_back),
-            contentDescription = "go to back",
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = "알림 설정",
-            style = ClodyTheme.typography.head4,
-            color = ClodyTheme.colors.gray01
-
-        )
-        Spacer(modifier = Modifier.weight(1f))
+    if (showBottomSheet) {
+        ClodyPopupBottomSheet(onDismissRequest = { showBottomSheet = false }) {
+            SettingBottomSheetTimePicker(
+                onDismissRequest = { showBottomSheet = false },
+                onTimeSelected = { newTime ->
+                    selectedTime = newTime
+                    showBottomSheet = false
+                }
+            )
+        }
     }
 }
 
@@ -100,10 +106,13 @@ fun DiaryWrittingAlarmSetting() {
 }
 
 @Composable
-fun AlarmTime(){
+fun AlarmTime(showBottomSheetStateChange: (Boolean) -> Unit) {
+    var selectedTime by remember { mutableStateOf("오후 9시 30분") }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .wrapContentHeight()
             .padding(horizontal = 24.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -114,20 +123,20 @@ fun AlarmTime(){
         )
         Spacer(modifier = Modifier.weight(1f))
         Text(
-            text = "오후 9시 30분",
+            text = selectedTime,
             style = ClodyTheme.typography.body3Medium,
-            color = ClodyTheme.colors.gray05
+            color = ClodyTheme.colors.gray05,
         )
         Image(
             painterResource(id = R.drawable.ic_setting_next),
             contentDescription = "pick the alarm time",
-
+            modifier = Modifier.clickable { showBottomSheetStateChange(true) }
         )
     }
 }
 
 @Composable
-fun ReplyAlarmSetting(){
+fun ReplyAlarmSetting() {
     val checkedState = remember { mutableStateOf(true) }
 
     Row(
@@ -154,7 +163,6 @@ fun ReplyAlarmSetting(){
             )
         )
     }
-
 }
 
 @Preview(showBackground = true)
