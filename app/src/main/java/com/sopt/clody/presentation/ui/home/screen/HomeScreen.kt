857f2f4cdd53d100f4fa00d66b06ed42621a8241
SYAAINN <alswo08319@gmail.com>
2024-07-16 00:16:03 +0900

    [UI/#26] 홈 화면 컴포넌트 추가

--- app/src/main/java/com/sopt/clody/presentation/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.clody.presentation.ui.home.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,35 +8,48 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sopt.clody.presentation.ui.component.YearMonthPicker
+import com.sopt.clody.presentation.ui.component.bottomsheet.DiaryDeleteSheet
+import com.sopt.clody.presentation.ui.component.dialog.ClodyDialog
+import com.sopt.clody.presentation.ui.component.popup.ClodyPopupBottomSheet
 import com.sopt.clody.presentation.ui.home.calendar.ClodyCalendar
 import com.sopt.clody.presentation.ui.home.component.CloverCount
 import com.sopt.clody.presentation.ui.home.component.DiaryStateButton
 import com.sopt.clody.presentation.ui.home.component.HomeTopAppBar
-import com.sopt.clody.presentation.ui.home.navigator.HomeNavigator
+import com.sopt.clody.presentation.ui.home.navigation.HomeNavigator
+import com.sopt.clody.ui.theme.ClodyTheme
 
 @Composable
 fun HomeRoute(
     navigator: HomeNavigator
 ) {
     HomeScreen(
-        onClickBack = { navigator.navigateBack() },
         onClickDiaryList = { navigator.navigateDiaryList() },
         onClickSetting = { navigator.navigateSetting() },
+        onClickWriteDiary = { navigator.navigateWriteDiary() },
         onClickReplyDiary = { navigator.navigateReplyDiary() }
     )
 }
 
 @Composable
 fun HomeScreen(
-    onClickBack: () -> Unit,
     onClickDiaryList: () -> Unit,
     onClickSetting: () -> Unit,
+    onClickWriteDiary: () -> Unit,
     onClickReplyDiary: () -> Unit,
 ) {
+    var showYearMonthPickerState by remember { mutableStateOf(false) }
+    var showDiaryDeleteState by remember { mutableStateOf(false) }
+    var showDiaryDeleteDialog by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -43,9 +57,22 @@ fun HomeScreen(
     ) {
         HomeTopAppBar(
             onClickDiaryList = onClickDiaryList,
-            onClickSetting = onClickSetting
+            onClickSetting = onClickSetting,
+            onShowYearMonthPickerStateChange = { newState -> showYearMonthPickerState = newState }
         )
-        ScrollableCalendarView()
+        ScrollableCalendarView(
+            onClickWriteDiary = onClickWriteDiary,
+            onClickReplyDiary = onClickReplyDiary,
+            onShowDiaryDeleteStateChange = { newState -> showDiaryDeleteState = newState }
+        )
+    }
+
+    if (showYearMonthPickerState) {
+        ClodyPopupBottomSheet(onDismissRequest = { showYearMonthPickerState = false }) {
+            YearMonthPicker(
+                onDismissRequest = { showYearMonthPickerState = false }
+            )
+        }
     }
 
     if (showDiaryDeleteState) {
@@ -70,18 +97,30 @@ fun HomeScreen(
 }
 
 @Composable
-fun ScrollableCalendarView() {
+fun ScrollableCalendarView(
+    onClickWriteDiary: () -> Unit,
+    onClickReplyDiary: () -> Unit,
+    onShowDiaryDeleteStateChange: (Boolean) -> Unit
+) {
     val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(scrollState)
+            .background(ClodyTheme.colors.white)
     ) {
         CloverCount()
         Spacer(modifier = Modifier.height(20.dp))
-        ClodyCalendar()
+        ClodyCalendar(
+            onShowDiaryDeleteStateChange = onShowDiaryDeleteStateChange
+        )
         Spacer(modifier = Modifier.height(14.dp))
-        DiaryStateButton(diaryCount = 5, replyStatus = "READY_NOT_READ")
+        DiaryStateButton(
+            diaryCount = 5,
+            replyStatus = "READY_NOT_READ",
+            onClickWriteDiary = onClickWriteDiary,
+            onClickReplyDiary = onClickReplyDiary
+        )
         Spacer(modifier = Modifier.height(14.dp))
     }
 }
