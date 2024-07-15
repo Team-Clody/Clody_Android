6d12979e6b7bd884e34f4e6b3a5f8cfd9bd28cbf
yeonjeen <jeendev2019@gmail.com>
2024-07-16 00:29:51 +0900

    [FEAT/#40] 월단위 캘린더 api get api 서비스 설정

--- app/src/main/java/com/sopt/clody/data/remote/api/ApiService.kt
+++ app/src/main/java/com/sopt/clody/data/remote/api/ApiService.kt
@@ -3,9 +3,11 @@ package com.sopt.clody.data.remote.api
 import com.sopt.clody.data.remote.dto.ExampleRequestDto
 import com.sopt.clody.data.remote.dto.base.ApiResponse
 import com.sopt.clody.data.remote.dto.ExampleResponseDto
+import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("api/v1/data")
@@ -14,4 +16,10 @@ interface ApiService {
     suspend fun postExampleData(
         @Body exampleRequestDto: ExampleRequestDto
     ): ApiResponse<Unit>
+
+    @GET("api/v1/calendar")
+    suspend fun getMonthlyCalendarData(
+        @Query("year") year: Int,
+        @Query("month") month: Int
+    ): ApiResponse<List<MonthlyCalendarResponseDto>>
 }
