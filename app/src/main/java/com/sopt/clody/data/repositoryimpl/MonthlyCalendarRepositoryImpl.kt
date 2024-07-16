3eea8aafbf673d30cf381c8c0f519150fff6183f
yeonjeen <jeendev2019@gmail.com>
2024-07-17 06:06:46 +0900

    [CHORE/#40] 캘린더 레포지터리 impl 설정

--- app/src/main/java/com/sopt/clody/data/repositoryimpl/MonthlyCalendarRepositoryImpl.kt
+++ app/src/main/java/com/sopt/clody/data/repositoryimpl/MonthlyCalendarRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.sopt.clody.data.repositoryimpl
 
 import com.sopt.clody.data.remote.datasource.MonthlyCalendarDataSource
-import com.sopt.clody.data.remote.dto.base.ApiResponse
 import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
 import com.sopt.clody.data.repository.MonthlyCalendarRepository
 import javax.inject.Inject
@@ -9,7 +8,14 @@ import javax.inject.Inject
 class MonthlyCalendarRepositoryImpl @Inject constructor(
     private val remoteDataSource: MonthlyCalendarDataSource
 ) : MonthlyCalendarRepository {
-    override suspend fun getMonthlyCalendarData(year: Int, month: Int): ApiResponse<List<MonthlyCalendarResponseDto>> {
-        return remoteDataSource.getCalendarData(year, month)
+    override suspend fun getMonthlyCalendarData(year: Int, month: Int): Result<MonthlyCalendarResponseDto> {
+        return runCatching {
+            val response = remoteDataSource.getCalendarData(year, month)
+            if (response.status == 200 && response.data != null) {
+                response.data
+            } else {
+                throw Exception(response.message)
+            }
+        }
     }
 }
