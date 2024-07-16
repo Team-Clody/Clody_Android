d85714399ae436e3fccb5bd1402c4c98ac494f9b
yeonjeen <jeendev2019@gmail.com>
2024-07-17 06:06:28 +0900

    [CHORE/#40] 캘린더 데이터소스 impl 설정

--- app/src/main/java/com/sopt/clody/data/remote/datasourceimpl/MonthlyCalendarDataSourcelmpl.kt
+++ app/src/main/java/com/sopt/clody/data/remote/datasourceimpl/MonthlyCalendarDataSourcelmpl.kt
@@ -1,15 +1,15 @@
 package com.sopt.clody.data.remote.datasourceimpl
 
-import com.sopt.clody.data.remote.api.ApiService
+import com.sopt.clody.data.remote.api.CalendarApiService
 import com.sopt.clody.data.remote.datasource.MonthlyCalendarDataSource
 import com.sopt.clody.data.remote.dto.base.ApiResponse
 import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
 import javax.inject.Inject
 
 class MonthlyCalendarDataSourceImpl @Inject constructor(
-    private val api: ApiService
+    private val api: CalendarApiService
 ) : MonthlyCalendarDataSource {
-    override suspend fun getCalendarData(year: Int, month: Int): ApiResponse<List<MonthlyCalendarResponseDto>> {
+    override suspend fun getCalendarData(year: Int, month: Int): ApiResponse<MonthlyCalendarResponseDto> {
         return api.getMonthlyCalendarData(year, month)
     }
 }
