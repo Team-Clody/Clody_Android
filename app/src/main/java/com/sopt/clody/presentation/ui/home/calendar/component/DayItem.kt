92d2b918e62cfd4fd1be1db7ec1739954e6ff59d
yeonjeen <jeendev2019@gmail.com>
2024-07-17 08:26:22 +0900

    [CHORE/#40] 불필요한 임포트문 삭제

--- app/src/main/java/com/sopt/clody/presentation/ui/home/calendar/component/DayItem.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/calendar/component/DayItem.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.clody.R
 import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
