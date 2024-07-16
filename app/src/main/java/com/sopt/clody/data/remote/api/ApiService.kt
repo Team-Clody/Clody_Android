c9b44e841b8ece9663c19b98857e4fbbb4915863
yeonjeen <jeendev2019@gmail.com>
2024-07-17 08:19:53 +0900

    [CHORE/#40] 불필요한 임포트문 삭제

--- app/src/main/java/com/sopt/clody/data/remote/api/ApiService.kt
+++ app/src/main/java/com/sopt/clody/data/remote/api/ApiService.kt
@@ -1,13 +1,11 @@
 package com.sopt.clody.data.remote.api
 
 import com.sopt.clody.data.remote.dto.ExampleRequestDto
-import com.sopt.clody.data.remote.dto.base.ApiResponse
 import com.sopt.clody.data.remote.dto.ExampleResponseDto
-import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
+import com.sopt.clody.data.remote.dto.base.ApiResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface ApiService {
     @GET("api/v1/data")
