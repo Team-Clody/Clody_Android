3f26ba44f4c55b18f65bbe9d69cb4f3aefcd3bee
SYAAINN <alswo08319@gmail.com>
2024-07-18 06:18:09 +0900

    [FEAT/#58] 닉네임 변경 Service 추가

--- app/src/main/java/com/sopt/clody/data/remote/api/AccountManagementService.kt
+++ app/src/main/java/com/sopt/clody/data/remote/api/AccountManagementService.kt
@@ -1,10 +1,19 @@
 package com.sopt.clody.data.remote.api
 
+import com.sopt.clody.data.remote.dto.RequestModifyNicknameDto
+import com.sopt.clody.data.remote.dto.ResponseModifyNicknameDto
 import com.sopt.clody.data.remote.dto.ResponseUserInfoDto
 import com.sopt.clody.data.remote.dto.base.ApiResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 
 interface AccountManagementService {
     @GET("api/v1/user/info")
     suspend fun getUserInfo(): ApiResponse<ResponseUserInfoDto>
+
+    @PATCH("api/v1/user/nickname")
+    suspend fun modifyNickname(
+        @Body body: RequestModifyNicknameDto
+    ) : ApiResponse<ResponseModifyNicknameDto>
 }
