83e11675452848c76667f264d607253d8ef399e2
Moonsu Kang <rkdms4971@naver.com>
2024-07-17 10:11:16 +0900

    [CHORE/#37] 의존성 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven(url = "https://jitpack.io")
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
