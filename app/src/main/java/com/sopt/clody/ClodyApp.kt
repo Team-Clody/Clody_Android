0bbf327a061cf13f1d35fe75b529eeda96216426
Moonsu Kang <rkdms4971@naver.com>
2024-07-17 10:16:09 +0900

    [ADD/#37] init kakaosdk

--- app/src/main/java/com/sopt/clody/ClodyApp.kt
+++ app/src/main/java/com/sopt/clody/ClodyApp.kt
@@ -1,6 +1,7 @@
 package com.sopt.clody
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,5 +10,9 @@ class ClodyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        initKakaoSdk()
+    }
+    private fun initKakaoSdk() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
     }
 }
