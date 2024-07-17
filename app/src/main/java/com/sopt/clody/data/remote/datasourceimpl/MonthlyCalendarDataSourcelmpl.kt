729817ad3dad4d253289976e0270528e12e4b74b
yeonjeen <jeendev2019@gmail.com>
2024-07-17 09:53:05 +0900

    [CHORE/#40] 변수명 변경

--- app/src/main/java/com/sopt/clody/data/remote/datasourceimpl/MonthlyCalendarDataSourcelmpl.kt
+++ app/src/main/java/com/sopt/clody/data/remote/datasourceimpl/MonthlyCalendarDataSourcelmpl.kt
@@ -7,9 +7,9 @@ import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
 import javax.inject.Inject
 
 class MonthlyCalendarDataSourceImpl @Inject constructor(
-    private val api: CalendarApiService
+    private val calendarApi: CalendarApiService
 ) : MonthlyCalendarDataSource {
     override suspend fun getCalendarData(year: Int, month: Int): ApiResponse<MonthlyCalendarResponseDto> {
-        return api.getMonthlyCalendarData(year, month)
+        return calendarApi.getMonthlyCalendarData(year, month)
     }
 }
