d58e1b31dafd1dbac562e80a5edcfc57b43eeadf
SYAAINN <alswo08319@gmail.com>
2024-07-18 08:33:40 +0900

    [FEAT/#63] 회원탈퇴 Service 생성

--- app/src/main/java/com/sopt/clody/data/remote/api/AccountManagementService.kt
+++ app/src/main/java/com/sopt/clody/data/remote/api/AccountManagementService.kt
@@ -2,9 +2,13 @@ package com.sopt.clody.data.remote.api
 
 import com.sopt.clody.data.remote.dto.ResponseUserInfoDto
 import com.sopt.clody.data.remote.dto.base.ApiResponse
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 
 interface AccountManagementService {
     @GET("api/v1/user/info")
     suspend fun getUserInfo(): ApiResponse<ResponseUserInfoDto>
+
+    @DELETE("api/v1/user/revoke")
+    suspend fun revoke() : ApiResponse<Unit>
 }
