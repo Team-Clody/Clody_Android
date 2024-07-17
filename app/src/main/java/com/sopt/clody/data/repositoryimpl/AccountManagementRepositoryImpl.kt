e7cc86fe9cdcc641128296c2d3aac829d13a1bce
SYAAINN <alswo08319@gmail.com>
2024-07-18 06:16:45 +0900

    [FEAT/#58] 닉네임 변경 repository 생성

--- app/src/main/java/com/sopt/clody/data/repositoryimpl/AccountManagementRepositoryImpl.kt
+++ app/src/main/java/com/sopt/clody/data/repositoryimpl/AccountManagementRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.sopt.clody.data.repositoryimpl
 
 import com.sopt.clody.data.remote.datasource.AccountManagementDataSource
+import com.sopt.clody.data.remote.dto.RequestModifyNicknameDto
+import com.sopt.clody.data.remote.dto.ResponseModifyNicknameDto
 import com.sopt.clody.data.remote.dto.ResponseUserInfoDto
 import com.sopt.clody.data.repository.AccountManagementRepository
 import com.sopt.clody.presentation.utils.extension.handleApiResponse
@@ -12,6 +14,14 @@ class AccountManagementRepositoryImpl @Inject constructor(
     override suspend fun getUserInfo(): Result<ResponseUserInfoDto> {
         return runCatching {
             accountManagementDataSource.getUserInfo().handleApiResponse().getOrThrow()
+        }
+    }
+
+    override suspend fun modifyNickname(
+        requestModifyNicknameDto: RequestModifyNicknameDto
+    ): Result<ResponseModifyNicknameDto> {
+        return runCatching {
+            accountManagementDataSource.ModifyNickname(requestModifyNicknameDto).handleApiResponse().getOrThrow()
         }
     }
 }
