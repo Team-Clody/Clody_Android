5e50c7c5bc5b769595aa8016a6b731c59f5c105b
SYAAINN <alswo08319@gmail.com>
2024-07-18 06:16:33 +0900

    [FEAT/#58] 닉네임 변경 dataSource 생성

--- app/src/main/java/com/sopt/clody/data/remote/datasourceimpl/AccountManagementDataSourceImpl.kt
+++ app/src/main/java/com/sopt/clody/data/remote/datasourceimpl/AccountManagementDataSourceImpl.kt
@@ -2,14 +2,20 @@ package com.sopt.clody.data.remote.datasourceimpl
 
 import com.sopt.clody.data.remote.api.AccountManagementService
 import com.sopt.clody.data.remote.datasource.AccountManagementDataSource
+import com.sopt.clody.data.remote.dto.RequestModifyNicknameDto
+import com.sopt.clody.data.remote.dto.ResponseModifyNicknameDto
 import com.sopt.clody.data.remote.dto.ResponseUserInfoDto
 import com.sopt.clody.data.remote.dto.base.ApiResponse
 import javax.inject.Inject
 
 class AccountManagementDataSourceImpl @Inject constructor(
-    private val accountManagementSevice: AccountManagementService
+    private val accountManagementSevice: AccountManagementService,
 ) : AccountManagementDataSource {
     override suspend fun getUserInfo(): ApiResponse<ResponseUserInfoDto> {
         return accountManagementSevice.getUserInfo()
+    }
+
+    override suspend fun ModifyNickname(requestModifyNicknameDto: RequestModifyNicknameDto): ApiResponse<ResponseModifyNicknameDto> {
+        return accountManagementSevice.modifyNickname(requestModifyNicknameDto)
     }
 }
