2f37da68e7877325daf438d8853746bbea18bf0f
Moonsu Kang <rkdms4971@naver.com>
2024-07-18 18:09:18 +0900

    [FEAT/#60] ReplyDiaryDto

---
+++ app/src/main/java/com/sopt/clody/data/remote/dto/ResponseReplyDiaryDto.kt
@@ -0,0 +1,12 @@
+package com.sopt.clody.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseReplyDiaryDto(
+    val content: String,
+    val nickname: String,
+    val month: Int,
+    val date: Int
+)
+
