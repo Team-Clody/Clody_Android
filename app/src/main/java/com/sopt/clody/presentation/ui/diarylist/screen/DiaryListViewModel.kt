097c845ae801e59d9abf58e830449b77cb36f8fa
yeonjeen <jeendev2019@gmail.com>
2024-07-19 05:05:28 +0900

    [FEAT/#85] 리스트 삭제 api 연결

--- app/src/main/java/com/sopt/clody/presentation/ui/diarylist/screen/DiaryListViewModel.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/diarylist/screen/DiaryListViewModel.kt
@@ -2,7 +2,9 @@ package com.sopt.clody.presentation.ui.diarylist.screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sopt.clody.data.repository.DailyDiaryListRepository
 import com.sopt.clody.data.repository.DiaryListRepository
+import com.sopt.clody.presentation.ui.home.screen.DeleteDiaryState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,11 +14,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DiaryListViewModel @Inject constructor(
-    private val diaryListRepository: DiaryListRepository
+    private val diaryListRepository: DiaryListRepository,
+    private val dailyDiaryListRepository: DailyDiaryListRepository
 ) : ViewModel() {
 
     private val _diaryListState = MutableStateFlow<DiaryListState>(DiaryListState.Idle)
     val diaryListState: StateFlow<DiaryListState> = _diaryListState
+
+    private val _deleteDiaryResult = MutableStateFlow<DeleteDiaryListState>(DeleteDiaryListState.Idle)
+    val deleteDiaryResult: StateFlow<DeleteDiaryListState> get() = _deleteDiaryResult
 
     fun fetchMonthlyDiary(year: Int, month: Int) {
         _diaryListState.value = DiaryListState.Loading
@@ -25,6 +31,21 @@ class DiaryListViewModel @Inject constructor(
             _diaryListState.value = result.fold(
                 onSuccess = { DiaryListState.Success(it) },
                 onFailure = { DiaryListState.Failure(it.message ?: "Unknown error") }
+            )
+        }
+    }
+
+    fun deleteDailyDiary(year: Int, month: Int, day: Int) {
+        viewModelScope.launch {
+            _deleteDiaryResult.value = DeleteDiaryListState.Loading
+            val result = dailyDiaryListRepository.deleteDailyDiary(year, month, day)
+            _deleteDiaryResult.value = result.fold(
+                onSuccess = {
+                    DeleteDiaryListState.Success
+                },
+                onFailure = {
+                    DeleteDiaryListState.Failure(it.message ?: "Unknown error")
+                }
             )
         }
     }
