6ebad0136fc8bbe6429203f0302c1c8e7a5d16f7
Moonsu Kang <rkdms4971@naver.com>
2024-07-19 01:38:30 +0900

    [REFACTOR/#75] 홈 네비 수정

--- app/src/main/java/com/sopt/clody/presentation/ui/home/navigation/HomeNavGraph.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/navigation/HomeNavGraph.kt
@@ -1,7 +1,9 @@
 package com.sopt.clody.presentation.ui.home.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.sopt.clody.presentation.ui.home.screen.HomeRoute
 import com.sopt.clody.presentation.ui.replyloading.navigation.ReplyLoadingNavigator
 import com.sopt.clody.presentation.ui.replyloading.screen.ReplyLoadingRoute
@@ -13,10 +15,18 @@ fun NavGraphBuilder.homeNavGraph(
     composable("home") {
         HomeRoute(navigator)
     }
-    composable("reply_loading/{year}/{month}/{day}") { backStackEntry ->
-        val year = backStackEntry.arguments?.getString("year")?.toIntOrNull() ?: 0
-        val month = backStackEntry.arguments?.getString("month")?.toIntOrNull() ?: 0
-        val day = backStackEntry.arguments?.getString("day")?.toIntOrNull() ?: 0
-        ReplyLoadingRoute(replyLoadingNavigator, year, month, day)
+    composable("reply_loading/{year}/{month}/{day}?from={from}",
+        arguments = listOf(
+            navArgument("year") { type = NavType.IntType },
+            navArgument("month") { type = NavType.IntType },
+            navArgument("day") { type = NavType.IntType },
+            navArgument("from") { defaultValue = "home" } // 기본값 설정
+        )
+    ) { backStackEntry ->
+        val year = backStackEntry.arguments?.getInt("year") ?: 0
+        val month = backStackEntry.arguments?.getInt("month") ?: 0
+        val day = backStackEntry.arguments?.getInt("day") ?: 0
+        val from = backStackEntry.arguments?.getString("from") ?: "home" // from 파라미터 가져오기
+        ReplyLoadingRoute(replyLoadingNavigator, year, month, day, from)
     }
 }
