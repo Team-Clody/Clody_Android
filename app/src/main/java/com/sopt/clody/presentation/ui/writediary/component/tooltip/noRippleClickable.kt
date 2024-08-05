ce488bc7acf01b2c19f506bb3042601df9507381
Moonsu Kang <rkdms4971@naver.com>
2024-08-06 04:02:22 +0900

    [ADD/#123] 클릭 가능 영역 만듦

---
+++ app/src/main/java/com/sopt/clody/presentation/ui/writediary/component/tooltip/noRippleClickable.kt
@@ -0,0 +1,17 @@
+package com.sopt.clody.presentation.ui.writediary.component.tooltip
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+@SuppressLint("ModifierFactoryUnreferencedReceiver")
+fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
+    }
+}
