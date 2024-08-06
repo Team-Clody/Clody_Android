0d620949f1a20618807b08ff51ede07f83b7cefe
SYAAINN <alswo08319@gmail.com>
2024-08-06 18:57:52 +0900

    [ADD/#125] "설정 > 앱 버전" 컴포넌트화

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/SettingScreen.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/SettingScreen.kt
@@ -72,24 +72,7 @@ fun SettingScreen(
             SettingOption(option = stringResource(R.string.setting_option_privacy_policy), { /* TODO : 개인정보 처리방침 이동 */ })
 
             Spacer(modifier = Modifier.height(20.dp))
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.setting_option_app_version),
-                    modifier = Modifier.padding(start = 4.dp),
-                    style = ClodyTheme.typography.body1Medium
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Text(
-                    text = stringResource(R.string.setting_app_version), /* TODO : 서버로부터 앱 버전 받아오기 */
-                    color = ClodyTheme.colors.gray05,
-                    style = ClodyTheme.typography.body4Medium
-                )
-            }
+            SettingAppVersion()
         }
     }
 }
