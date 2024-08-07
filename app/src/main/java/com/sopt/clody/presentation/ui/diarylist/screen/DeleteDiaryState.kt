f1d150418437b3c64edacda01ee4d3e1d9c011f4
SYAAINN <alswo08319@gmail.com>
2024-08-07 10:37:31 +0900

    [CHORE/#132] DeleteDiaryState -> DiaryDeleteState 네이밍 수정

--- app/src/main/java/com/sopt/clody/presentation/ui/diarylist/screen/DeleteDiaryState.kt
+++
@@ -1,8 +0,0 @@
-package com.sopt.clody.presentation.ui.diarylist.screen
-
-sealed class DeleteDiaryState {
-    data object Idle : DeleteDiaryState()
-    data object Loading : DeleteDiaryState()
-    data object Success : DeleteDiaryState()
-    data class Failure(val errorMessage: String) : DeleteDiaryState()
-}
