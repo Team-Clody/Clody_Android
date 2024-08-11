2636c7f80af490faf37b4e0caf98729123038792
SYAAINN <alswo08319@gmail.com>
2024-08-11 21:21:48 +0900

    [REFACTOR/#141] runCatching을 사용하여 버전을 불러오는데 실패했을 경우 대응

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/SettingViewModel.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/SettingViewModel.kt
@@ -6,23 +6,30 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.clody.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingViewModel @Inject constructor(
     @ApplicationContext private val context: Context
 ) : ViewModel() {
-    var versionInfo by mutableStateOf(APP_VERSION)
+    var versionInfo by mutableStateOf<String?>(null)
         private set
 
     fun getVersionInfo() {
-        val info: PackageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-        versionInfo = info.versionName
-    }
-
-    companion object {
-        const val APP_VERSION = "최신버전"
+        viewModelScope.launch {
+            runCatching {
+                val info: PackageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+                info.versionName
+            }.onSuccess { versionName ->
+                versionInfo = versionName
+            }.onFailure {
+                versionInfo = context.getString(R.string.setting_option_app_version_info_failure)
+            }
+        }
     }
 }
