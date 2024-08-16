61d788fc5a3fdb74375d22df6532223f51a7c090
yeonjeen <jeendev2019@gmail.com>
2024-08-17 07:18:41 +0900

    [FIX/#158] 변수명 수정

--- app/src/main/java/com/sopt/clody/presentation/ui/home/calendar/ClodyCalendar.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/calendar/ClodyCalendar.kt
@@ -1,25 +1,14 @@
 package com.sopt.clody.presentation.ui.home.calendar
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sopt.clody.data.remote.dto.response.MonthlyCalendarResponseDto
@@ -31,7 +20,6 @@ import com.sopt.clody.presentation.ui.home.calendar.component.HorizontalDivider
 import com.sopt.clody.presentation.ui.home.calendar.component.MonthlyItem
 import com.sopt.clody.presentation.ui.home.screen.DailyDiariesState
 import com.sopt.clody.presentation.ui.home.screen.HomeViewModel
-import com.sopt.clody.ui.theme.ClodyTheme
 import java.time.LocalDate
 import java.time.YearMonth
 
@@ -52,7 +40,7 @@ fun ClodyCalendar(
     }
     val initialDayOfWeek = selectedDate.dayOfWeek
 
-    val dailyDiariesUiState by homeViewModel.dailyDiariesUiState.collectAsStateWithLifecycle()
+    val dailyDiariesUiState by homeViewModel.dailyDiariesState.collectAsStateWithLifecycle()
 
     Column(
         modifier = Modifier
