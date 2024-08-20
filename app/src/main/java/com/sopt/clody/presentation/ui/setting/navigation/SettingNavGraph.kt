afe733af6fa050985b274ca667aa0a7f123ab9d4
SYAAINN <alswo08319@gmail.com>
2024-08-21 00:06:59 +0900

    [FEAT/#157] WebViewScreen 네비게이션 연결

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/navigation/SettingNavGraph.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/navigation/SettingNavGraph.kt
@@ -1,10 +1,13 @@
 package com.sopt.clody.presentation.ui.setting.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.sopt.clody.presentation.ui.setting.screen.AccountManagementRoute
 import com.sopt.clody.presentation.ui.setting.screen.NotificationSettingRoute
 import com.sopt.clody.presentation.ui.setting.screen.SettingRoute
+import com.sopt.clody.presentation.ui.setting.screen.WebViewRoute
 
 fun NavGraphBuilder.settingNavGraph(
     navigator: SettingNavigator
@@ -27,5 +30,19 @@ fun NavGraphBuilder.notificationSettingNavGraph(
 ) {
     composable("notification_setting") {
         NotificationSettingRoute(navigator)
+    }
+}
+
+fun NavGraphBuilder.webViewNavGraph(
+    navigator: SettingNavigator
+) {
+    composable(
+        route = "web_view/{encodeUrl}",
+        arguments = listOf(navArgument("encodeUrl") { type = NavType.StringType })
+    ) { backStackEntry ->
+        val encodeUrl = backStackEntry.arguments?.getString("encodeUrl")
+        encodeUrl?.let {
+            WebViewRoute(navigator, it)
+        }
     }
 }
--- app/src/main/java/com/sopt/clody/presentation/ui/setting/navigation/SettingNavigator.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/navigation/SettingNavigator.kt
@@ -1,6 +1,7 @@
 package com.sopt.clody.presentation.ui.setting.navigation
 
 import androidx.navigation.NavHostController
+import java.net.URLEncoder
 
 class SettingNavigator(
     val navController: NavHostController
@@ -11,6 +12,11 @@ class SettingNavigator(
 
     fun navigateNotificationSetting() {
         navController.navigate("notification_setting")
+    }
+
+    fun navigateWebView(url: String) {
+        val encodedUrl = URLEncoder.encode(url, "UTF-8")
+        navController.navigate("web_view/$encodedUrl")
     }
 
     fun navigateBack() {
