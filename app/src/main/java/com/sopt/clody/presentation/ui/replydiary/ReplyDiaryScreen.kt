f64ddce3602dc197205c7cc0deb905440b7d8dbc
Moonsu Kang <rkdms4971@naver.com>
2024-08-21 19:48:49 +0900

    [FEAT/#165] 에러대응 화면 적용

--- app/src/main/java/com/sopt/clody/presentation/ui/replydiary/ReplyDiaryScreen.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/replydiary/ReplyDiaryScreen.kt
@@ -3,7 +3,6 @@ package com.sopt.clody.presentation.ui.replydiary
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,11 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -32,10 +29,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sopt.clody.R
+import com.sopt.clody.presentation.ui.component.FailureScreen
+import com.sopt.clody.presentation.ui.component.LoadingScreen
 import com.sopt.clody.presentation.ui.replydiary.navigation.ReplyDiaryNavigator
 import com.sopt.clody.ui.theme.ClodyTheme
 
@@ -89,71 +87,74 @@ fun ReplyDiaryScreen(
         }
     }
 
-    if (showDialog) {
-        if (replyDiaryState is ReplyDiaryState.Success) {
-            val nickname = (replyDiaryState as ReplyDiaryState.Success).nickname
-            CloverDialog(
-                onDismiss = { showDialog = false },
-                titleMassage = "${nickname}님을 위한 행운 도착",
-                descriptionMassage = "1개의 네잎클로버 획득",
-                confirmOption = "확인",
-                confirmAction = {
-                    showDialog = false
-                },
-                confirmButtonColor = ClodyTheme.colors.mainYellow
+    when (replyDiaryState) {
+        is ReplyDiaryState.Loading -> {
+            LoadingScreen()
+        }
+
+        is ReplyDiaryState.Failure -> {
+            FailureScreen(
+                message = (replyDiaryState as ReplyDiaryState.Failure).error,
+                confirmAction = { viewModel.retryLastRequest() }
             )
         }
-    } else {
-        Scaffold(
-            topBar = {
-                if (replyDiaryState is ReplyDiaryState.Success || replyDiaryState is ReplyDiaryState.NotFound) {
-                    val month = (replyDiaryState as? ReplyDiaryState.Success)?.month
-                    val date = (replyDiaryState as? ReplyDiaryState.Success)?.date
-                    CenterAlignedTopAppBar(
-                        title = {
-                            Text(
-                                text = "${month}월 ${date}일",
-                                style = ClodyTheme.typography.head4,
-                                color = ClodyTheme.colors.gray01,
-                            )
-                        },
-                        navigationIcon = {
-                            IconButton(onClick = onClickBack) {
-                                Image(
-                                    painterResource(id = R.drawable.ic_nickname_back),
-                                    contentDescription = "back"
+
+        is ReplyDiaryState.Success -> {
+            if (showDialog) {
+                val nickname = (replyDiaryState as ReplyDiaryState.Success).nickname
+                CloverDialog(
+                    onDismiss = { showDialog = false },
+                    titleMassage = "${nickname}님을 위한 행운 도착",
+                    descriptionMassage = "1개의 네잎클로버 획득",
+                    confirmOption = "확인",
+                    confirmAction = {
+                        showDialog = false
+                    },
+                    confirmButtonColor = ClodyTheme.colors.mainYellow
+                )
+            } else {
+                Scaffold(
+                    topBar = {
+                        val month = (replyDiaryState as ReplyDiaryState.Success).month
+                        val date = (replyDiaryState as ReplyDiaryState.Success).date
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(
+                                    text = "${month}월 ${date}일",
+                                    style = ClodyTheme.typography.head4,
+                                    color = ClodyTheme.colors.gray01,
                                 )
-                            }
-                        },
-                        colors = TopAppBarDefaults.topAppBarColors(ClodyTheme.colors.white),
-                    )
-                }
-            },
-            content = { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(ClodyTheme.colors.white)
-                        .padding(innerPadding)
-                        .padding(horizontal = 24.dp)
-                ) {
-                    Spacer(modifier = Modifier.height(13.dp))
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = onClickBack) {
+                                    Image(
+                                        painterResource(id = R.drawable.ic_nickname_back),
+                                        contentDescription = "back"
+                                    )
+                                }
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(ClodyTheme.colors.white),
+                        )
+                    },
+                    content = { innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(ClodyTheme.colors.white)
+                                .padding(innerPadding)
+                                .padding(horizontal = 24.dp)
+                        ) {
+                            Spacer(modifier = Modifier.height(13.dp))
 
-                    Box(
-                        contentAlignment = Alignment.TopCenter,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 28.dp)
-                            .clip(RoundedCornerShape(16.dp))
-                            .background(ClodyTheme.colors.gray08)
-                            .weight(1f)
-                    ) {
-                        when (replyDiaryState) {
-                            is ReplyDiaryState.Loading -> {
-                                CircularProgressIndicator()
-                            }
-
-                            is ReplyDiaryState.Success -> {
+                            Box(
+                                contentAlignment = Alignment.TopCenter,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(bottom = 28.dp)
+                                    .clip(RoundedCornerShape(16.dp))
+                                    .background(ClodyTheme.colors.gray08)
+                                    .weight(1f)
+                            ) {
                                 val content = (replyDiaryState as ReplyDiaryState.Success).content
                                 val nickname = (replyDiaryState as ReplyDiaryState.Success).nickname
                                 LazyColumn(
@@ -198,40 +199,12 @@ fun ReplyDiaryScreen(
                                     }
                                 }
                             }
-
-                            is ReplyDiaryState.Failure -> {
-                                val error = (replyDiaryState as ReplyDiaryState.Failure).error
-                                Text("Error: $error")
-                            }
-
-                            is ReplyDiaryState.NotFound -> {
-                                Column(
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    verticalArrangement = Arrangement.Center,
-                                    modifier = Modifier.fillMaxSize()
-                                ) {
-                                    Image(
-                                        painter = painterResource(id = R.drawable.ic_not_found),
-                                        contentDescription = null,
-                                        Modifier.size(80.dp)
-                                    )
-                                    Text(
-                                        text = "현재 시스템에러로 답장을 작성하지 못했어요 \n잠시 후 다시 확인해주세요",
-                                        style = ClodyTheme.typography.head4,
-                                        color = ClodyTheme.colors.gray01,
-                                        textAlign = TextAlign.Center,
-                                        modifier = Modifier
-                                            .padding(top = 16.dp)
-                                            .align(Alignment.CenterHorizontally)
-                                    )
-                                }
-                            }
-
-                            else -> {}
                         }
                     }
-                }
+                )
             }
-        )
+        }
+
+        else -> {}
     }
 }
