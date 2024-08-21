5accf4e8b6a45618199bcdf9b47e76bb3525f1c9
Moonsu Kang <rkdms4971@naver.com>
2024-08-21 18:06:01 +0900

    [FEAT/#165] 알림 설정 에러대응 적용

--- app/src/main/java/com/sopt/clody/presentation/ui/auth/timereminder/TimeReminderViewModel.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/auth/timereminder/TimeReminderViewModel.kt
@@ -8,6 +8,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.clody.data.remote.dto.request.RequestSendNotificationDto
 import com.sopt.clody.data.repository.NotificationRepository
+import com.sopt.clody.presentation.utils.network.ErrorMessages.FAILURE_NETWORK_MESSAGE
+import com.sopt.clody.presentation.utils.network.ErrorMessages.FAILURE_TEMPORARY_MESSAGE
+import com.sopt.clody.presentation.utils.network.ErrorMessages.UNKNOWN_ERROR
+import com.sopt.clody.presentation.utils.network.NetworkUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,7 +20,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class TimeReminderViewModel @Inject constructor(
-    private val notificationRepository: NotificationRepository
+    private val notificationRepository: NotificationRepository,
+    private val networkUtil: NetworkUtil
 ) : ViewModel() {
 
     private val _timeReminderState = MutableStateFlow<TimeReminderState>(TimeReminderState.Idle)
@@ -27,6 +32,11 @@ class TimeReminderViewModel @Inject constructor(
 
     fun sendNotification(context: Context, isPermissionGranted: Boolean) {
         viewModelScope.launch {
+            if (!networkUtil.isNetworkAvailable()) {
+                _timeReminderState.value = TimeReminderState.Failure(FAILURE_NETWORK_MESSAGE)
+                return@launch
+            }
+
             val fcmToken = getTokenFromPreferences(context)
             if (fcmToken.isNullOrBlank()) {
                 _timeReminderState.value = TimeReminderState.Failure("FCM 못가져옴")
@@ -45,11 +55,20 @@ class TimeReminderViewModel @Inject constructor(
                 onSuccess = {
                     _timeReminderState.value = TimeReminderState.Success("알림 전송 성공")
                 },
-                onFailure = {
-                    _timeReminderState.value = TimeReminderState.Failure(it.message ?: "알림 전송 실패")
+                onFailure = { error ->
+                    val errorMessage = if (error.message?.contains("200") == false) {
+                        FAILURE_TEMPORARY_MESSAGE
+                    } else {
+                        error.localizedMessage ?: UNKNOWN_ERROR
+                    }
+                    _timeReminderState.value = TimeReminderState.Failure(errorMessage)
                 }
             )
         }
+    }
+
+    fun resetTimeReminderState() {
+        _timeReminderState.value = TimeReminderState.Idle
     }
 
     private fun getTokenFromPreferences(context: Context): String? {
