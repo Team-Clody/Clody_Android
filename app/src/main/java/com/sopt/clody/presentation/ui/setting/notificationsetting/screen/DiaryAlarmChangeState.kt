822ce5d1d592b8581e6144bed846768259050cc1
SYAAINN <alswo08319@gmail.com>
2024-08-22 19:56:43 +0900

    [ADD/#151] 서버 통신 State 추가

---
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/notificationsetting/screen/DiaryAlarmChangeState.kt
@@ -0,0 +1,10 @@
+package com.sopt.clody.presentation.ui.setting.notificationsetting.screen
+
+import com.sopt.clody.data.remote.dto.response.ResponseSendNotificationDto
+
+sealed class DiaryAlarmChangeState {
+    data object Idle: DiaryAlarmChangeState()
+    data object Loading: DiaryAlarmChangeState()
+    data class Success(val data: ResponseSendNotificationDto): DiaryAlarmChangeState()
+    data class Failure(val errorMessage: String): DiaryAlarmChangeState()
+}
