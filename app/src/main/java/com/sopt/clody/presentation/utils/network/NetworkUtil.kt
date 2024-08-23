b0945ad70c428fb6e301021c279ee205ba6526ca
SYAAINN <alswo08319@gmail.com>
2024-08-24 00:12:26 +0900

    [ADD/#174] NetworkModule, NetworkUtil 추가

---
+++ app/src/main/java/com/sopt/clody/presentation/utils/network/NetworkUtil.kt
@@ -0,0 +1,13 @@
+package com.sopt.clody.presentation.utils.network
+
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkUtil(private val connectivityManager: ConnectivityManager) {
+
+    fun isNetworkAvailable(): Boolean {
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+    }
+}
