8eafaa887669e67a914757ec9e08595bee11be48
Minjae Son <137160756+SYAAINN@users.noreply.github.com>
2024-08-30 11:12:47 +0900

    [CHORE/#175] 알림설정 스위치 크기 조정 및 thumb 에셋 추가

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/notificationsetting/component/DiaryAlarmSwitch.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/notificationsetting/component/DiaryAlarmSwitch.kt
@@ -1,9 +1,11 @@
 package com.sopt.clody.presentation.ui.setting.notificationsetting.component
 
 import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
@@ -12,7 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.sopt.clody.R
 import com.sopt.clody.data.remote.dto.response.ResponseNotificationInfoDto
 import com.sopt.clody.presentation.ui.setting.notificationsetting.screen.NotificationSettingViewModel
 import com.sopt.clody.ui.theme.ClodyTheme
@@ -42,6 +47,13 @@ fun DiaryAlarmSwitch(
             onCheckedChange = {
                 checkedState.value = it
                 notificationSettingViewModel.changeDiaryAlarm(context, notificationInfo, checkedState.value)
+            },
+            modifier = Modifier.scale(0.8f),
+            thumbContent = {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_notification_setting_switch_thumb),
+                    contentDescription = null
+                )
             },
             colors = SwitchDefaults.colors(
                 checkedThumbColor = ClodyTheme.colors.white,
