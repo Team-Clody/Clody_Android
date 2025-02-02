86f69a5a99162b0dde2a1c76f30c03fc7413e72e
Moonsu Kang <rkdms4971@naver.com>
2025-02-03 00:07:19 +0900

    [add/#212] app update dependency 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -168,4 +168,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     //fcm
     implementation(libs.firebase.messaging.ktx)
+
+    implementation(libs.app.update)
+    implementation(libs.app.update.ktx)
 }
