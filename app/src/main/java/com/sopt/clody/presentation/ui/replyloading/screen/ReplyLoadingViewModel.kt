e680fac46b107b05fef149c95b67dd5d2d04cd3c
MoonsuKang <rkdms4971@naver.com>
2025-03-15 18:36:23 +0900

    [ADD/#224] 광고 시청 완료 후 일기 즉시 조회 기능 추가

--- app/src/main/java/com/sopt/clody/presentation/ui/replyloading/screen/ReplyLoadingViewModel.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/replyloading/screen/ReplyLoadingViewModel.kt
@@ -1,10 +1,14 @@
 package com.sopt.clody.presentation.ui.replyloading.screen
 
+import android.app.Activity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sopt.clody.core.RewardAdShower
 import com.sopt.clody.data.remote.dto.response.DiaryTimeResponseDto
 import com.sopt.clody.data.remote.util.NetworkUtil
+import com.sopt.clody.domain.repository.AdRepository
 import com.sopt.clody.domain.repository.DiaryRepository
+import com.sopt.clody.domain.usecase.LoadRewardedAdUseCase
 import com.sopt.clody.presentation.utils.extension.throttleFirst
 import com.sopt.clody.presentation.utils.network.ErrorMessages.FAILURE_NETWORK_MESSAGE
 import com.sopt.clody.presentation.utils.network.ErrorMessages.FAILURE_TEMPORARY_MESSAGE
@@ -22,12 +26,18 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ReplyLoadingViewModel @Inject constructor(
+    private val loadRewardedAdUseCase: LoadRewardedAdUseCase,
     private val diaryRepository: DiaryRepository,
-    private val networkUtil: NetworkUtil
+    private val adRepository: AdRepository,
+    private val networkUtil: NetworkUtil,
+    private val rewardAdShower: RewardAdShower
 ) : ViewModel() {
 
     private val _replyLoadingState = MutableStateFlow<ReplyLoadingState>(ReplyLoadingState.Idle)
     val replyLoadingState: StateFlow<ReplyLoadingState> = _replyLoadingState
+
+    private val _isAdLoading = MutableStateFlow(false)
+    val isAdLoading: StateFlow<Boolean> = _isAdLoading
 
     private var lastYear: Int = 0
     private var lastMonth: Int = 0
@@ -73,10 +83,14 @@ class ReplyLoadingViewModel @Inject constructor(
         result.fold(
             onSuccess = { data ->
                 val diaryWrittenDay = data.date.split("-")
-                val targetDateTime = LocalDateTime.of(
+                var targetDateTime = LocalDateTime.of(
                     diaryWrittenDay[0].toInt(), diaryWrittenDay[1].toInt(), diaryWrittenDay[2].toInt(),
                     data.HH, data.mm, data.ss
                 ).plusMinutes(if (data.isFirst) INITIAL_REMINDER_MINUTES else REGULAR_REMINDER_HOURS * 60)
+
+                if (data.isFromAd) {
+                    targetDateTime = LocalDateTime.now()
+                }
 
                 _replyLoadingState.value = ReplyLoadingState.Success(targetDateTime)
             },
@@ -84,6 +98,58 @@ class ReplyLoadingViewModel @Inject constructor(
                 _replyLoadingState.value = ReplyLoadingState.Failure(FAILURE_TEMPORARY_MESSAGE)
                 val errorMessage = throwable.localizedMessage ?: UNKNOWN_ERROR
                 Timber.tag("ReplyLoadingViewModel").e("API 요청 실패: %s", errorMessage)
+            }
+        )
+    }
+
+    //  광고 시작 → 광고 시청 → 광고 종료 후 응답 업데이트
+    fun loadAndShowRewardedAd(activity: Activity) {
+        if (_isAdLoading.value) return
+
+        _isAdLoading.value = true
+        viewModelScope.launch {
+            Timber.d("광고 시작 API 호출: year=$lastYear, month=$lastMonth, day=$lastDate")
+
+            val startAdResult = adRepository.startAd(lastYear, lastMonth, lastDate)
+
+            startAdResult.onFailure {
+                _isAdLoading.value = false
+                _replyLoadingState.value = ReplyLoadingState.Failure("광고 시작 실패: ${it.localizedMessage}")
+                Timber.e("광고 시작 실패: ${it.localizedMessage}")
+                return@launch
+            }
+
+            Timber.d("광고 시작 성공, 광고 로드 시작")
+
+            loadRewardedAdUseCase().onSuccess {
+                _isAdLoading.value = false
+                showRewardedAdAndReloadDiaryTime(activity)
+            }.onFailure {
+                _isAdLoading.value = false
+                _replyLoadingState.value = ReplyLoadingState.Failure("광고 로드 실패")
+                Timber.e("로드 실패 사유: ${it.localizedMessage}")
+            }
+        }
+    }
+
+
+    private fun showRewardedAdAndReloadDiaryTime(activity: Activity) {
+        rewardAdShower.showAd(
+            activity,
+            onAdRewarded = {
+                viewModelScope.launch {
+                    adRepository.endAd(lastYear, lastMonth, lastDate).onFailure {
+                        Timber.e("종료 실패")
+                        return@launch
+                    }
+
+                    Timber.d("시간 조회 시작")
+                    getDiaryTimeInternal(lastYear, lastMonth, lastDate)
+                }
+            },
+            onAdDismissed = {
+                Timber.e("광고가 준비되지 않았습니다.")
+                // :TODO 광고 준비 실패 시 처리(Toast Message)
             }
         )
     }
