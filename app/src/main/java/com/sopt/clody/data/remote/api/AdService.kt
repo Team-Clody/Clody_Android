6e0addc6097e49af28e6db77f4eee4fceea0f46d
MoonsuKang <rkdms4971@naver.com>
2025-03-20 19:52:34 +0900

    [MOD/#224] BaseResponse -> NullableApiResponse

--- app/src/main/java/com/sopt/clody/data/remote/api/AdService.kt
+++ app/src/main/java/com/sopt/clody/data/remote/api/AdService.kt
@@ -1,6 +1,6 @@
 package com.sopt.clody.data.remote.api
 
-import com.sopt.clody.data.remote.dto.base.BaseResponse
+import com.sopt.clody.data.remote.dto.base.NullableApiResponse
 import com.sopt.clody.data.remote.dto.request.AdRequestDto
 import retrofit2.http.Body
 import retrofit2.http.PATCH
@@ -10,10 +10,10 @@ interface AdService {
     @POST("api/v1/reply/ad/start")
     suspend fun startAd(
         @Body adRequestDto: AdRequestDto
-    ) : BaseResponse<Unit>
+    ) : NullableApiResponse<Unit>
 
     @PATCH("api/v1/reply/ad/end")
     suspend fun endAd(
         @Body adRequestDto: AdRequestDto
-    ) : BaseResponse<Unit>
+    ) : NullableApiResponse<Unit>
 }
--- app/src/main/java/com/sopt/clody/data/remote/dto/base/NullableApiResponse.kt
+++ app/src/main/java/com/sopt/clody/data/remote/dto/base/NullableApiResponse.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class BaseResponse<T>(
+data class NullableApiResponse<T>(
     @SerialName("status") val status: Int,
     @SerialName("message") val message: String,
     @SerialName("data") val data: T? = null,
