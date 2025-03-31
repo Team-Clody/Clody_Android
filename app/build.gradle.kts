a47458da7c7de16f6b12b2641b753ceea4343708
MoonsuKang <rkdms4971@naver.com>
2025-03-31 19:49:58 +0900

    [ADD] 테스트 환경을 위한 debug signing configuration 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
