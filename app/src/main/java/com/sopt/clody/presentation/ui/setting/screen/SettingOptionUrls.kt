189557d278281f98a16e0ded04a831a0237498ca
SYAAINN <alswo08319@gmail.com>
2025-03-31 15:38:05 +0900

    [CHORE/#243] 변경된 링크를 반영합니다.

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/SettingOptionUrls.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/screen/SettingOptionUrls.kt
@@ -1,8 +1,8 @@
 package com.sopt.clody.presentation.ui.setting.screen
 
 object SettingOptionUrls {
-    const val ANNOUNCEMENT_URL = "https://phrygian-open-50e.notion.site/800f9d9e139740409ec1ebc6da0339e0?pvs=4"
+    const val ANNOUNCEMENT_URL = "https://www.notion.so/1c7e3fedb3f48029b36cf9d76c5fb6d6?pvs=21"
     const val INQUIRIES_SUGGESTIONS_URL = "https://forms.gle/WnLC7VwHacufVHiv7"
-    const val TERMS_OF_SERVICE_URL = "https://phrygian-open-50e.notion.site/4b8254c57c124f37afe3302ca7dd33c2?pvs=4"
-    const val PRIVACY_POLICY_URL = "https://phrygian-open-50e.notion.site/21cb8d6027404b2aa05741bcf67a4503?pvs=4"
+    const val TERMS_OF_SERVICE_URL = "https://www.notion.so/1c7e3fedb3f4802c8db1f3056c03973f?pvs=21"
+    const val PRIVACY_POLICY_URL = "https://www.notion.so/1c7e3fedb3f48024a334c8116255b378?pvs=21"
 }
