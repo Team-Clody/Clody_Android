6e1c71c1f84e7bf4528233710ee5b17bcdab422d
MoonsuKang <rkdms4971@naver.com>
2025-04-03 16:10:14 +0900

    [REFACTOR/#238] RemoteConfigDataSource 개선
    - RemoteConfig key 값 companion object로 상수화
    - RemoteConfig 값 미존재 시 현재 앱 버전으로 fallback 처리

--- app/src/main/java/com/sopt/clody/data/remote/datasource/RemoteConfigDataSource.kt
+++ app/src/main/java/com/sopt/clody/data/remote/datasource/RemoteConfigDataSource.kt
@@ -1,6 +1,7 @@
 package com.sopt.clody.data.remote.datasource
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.sopt.clody.BuildConfig
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -13,6 +14,14 @@ class RemoteConfigDataSource @Inject constructor(
         remoteConfig.fetchAndActivate().await()
     }
 
-    fun getLatestVersion(): String = remoteConfig.getString("latest_version")
-    fun getMinimumVersion(): String = remoteConfig.getString("min_required_version")
+    fun getLatestVersion(): String =
+        remoteConfig.getString(KEY_LATEST_VERSION).ifEmpty { BuildConfig.VERSION_NAME }
+
+    fun getMinimumVersion(): String =
+        remoteConfig.getString(KEY_MINIMUM_VERSION).ifEmpty { BuildConfig.VERSION_NAME }
+
+    companion object {
+        private const val KEY_LATEST_VERSION = "latest_version"
+        private const val KEY_MINIMUM_VERSION = "minimum_version"
+    }
 }
