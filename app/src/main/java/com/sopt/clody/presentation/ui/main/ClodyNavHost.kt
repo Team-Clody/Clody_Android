4fdf85b725dc5420be42bee37eb139fb7b8bfc51
MoonsuKang <rkdms4971@naver.com>
2025-05-07 01:07:45 +0900

    [ADD/#260] ClodyNavHost 컴포저블 추가(앱 전역 내비게이션 구성)

---
+++ app/src/main/java/com/sopt/clody/presentation/ui/main/ClodyNavHost.kt
@@ -0,0 +1,130 @@
+package com.sopt.clody.presentation.ui.main
+
+import android.content.Intent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.sopt.clody.presentation.ui.auth.guide.guideScreen
+import com.sopt.clody.presentation.ui.auth.guide.navigateToGuide
+import com.sopt.clody.presentation.ui.auth.login.navigation.loginScreen
+import com.sopt.clody.presentation.ui.auth.login.navigation.navigateToLogin
+import com.sopt.clody.presentation.ui.auth.signup.navigation.navigateToNickname
+import com.sopt.clody.presentation.ui.auth.signup.navigation.navigateToTermsOfService
+import com.sopt.clody.presentation.ui.auth.signup.navigation.nicknameScreen
+import com.sopt.clody.presentation.ui.auth.signup.navigation.termsOfServiceScreen
+import com.sopt.clody.presentation.ui.auth.timereminder.navigateToTimeReminder
+import com.sopt.clody.presentation.ui.auth.timereminder.timeReminderScreen
+import com.sopt.clody.presentation.ui.diarylist.navigation.diaryListScreen
+import com.sopt.clody.presentation.ui.diarylist.navigation.navigateToDiaryList
+import com.sopt.clody.presentation.ui.home.navigation.homeScreen
+import com.sopt.clody.presentation.ui.home.navigation.navigateToHome
+import com.sopt.clody.presentation.ui.replydiary.navigation.navigateToReplyDiary
+import com.sopt.clody.presentation.ui.replydiary.navigation.replyDiaryScreen
+import com.sopt.clody.presentation.ui.replyloading.navigation.navigateToReplyLoading
+import com.sopt.clody.presentation.ui.replyloading.navigation.replyLoadingScreen
+import com.sopt.clody.presentation.ui.setting.navigation.accountManagementScreen
+import com.sopt.clody.presentation.ui.setting.navigation.navigateToAccountManagement
+import com.sopt.clody.presentation.ui.setting.navigation.navigateToNotificationSetting
+import com.sopt.clody.presentation.ui.setting.navigation.navigateToSetting
+import com.sopt.clody.presentation.ui.setting.navigation.navigateToWebView
+import com.sopt.clody.presentation.ui.setting.navigation.notificationSettingScreen
+import com.sopt.clody.presentation.ui.setting.navigation.settingScreen
+import com.sopt.clody.presentation.ui.setting.navigation.webViewScreen
+import com.sopt.clody.presentation.ui.splash.navigation.splashScreen
+import com.sopt.clody.presentation.ui.writediary.navigation.navigateToWriteDiary
+import com.sopt.clody.presentation.ui.writediary.navigation.writeDiaryScreen
+import com.sopt.clody.presentation.utils.navigation.safePopBackStack
+
+/**
+ * 앱 전역의 Navigation Host.
+ *
+ * 각 기능? 화면?의 navigation graph를 이곳에 연결.
+ * [NavHost]를 사용하여 화면 전환을 구성하고,
+ * startIntent 등을 통해 초기 경로 조건 처리도 할 수 있음.
+ *
+ * @param appState 앱 상태를 포함한 네비게이션 컨트롤러
+ * @param modifier Compose UI Modifier
+ * @param startIntent 외부에서 전달된 인텐트 (푸시 처리 등)
+ */
+@Composable
+fun ClodyNavHost(
+    appState: ClodyAppState,
+    modifier: Modifier = Modifier,
+    startIntent: Intent,
+) {
+    val navController: NavHostController = appState.navController
+
+    Box(modifier = modifier.fillMaxSize()) {
+        NavHost(
+            navController = navController,
+            startDestination = appState.startDestination,
+        ) {
+            splashScreen(
+                startIntent = startIntent,
+                navigateToLogin = navController::navigateToLogin,
+                navigateToHome = navController::navigateToHome,
+            )
+
+            loginScreen(
+                navigateToTerms = navController::navigateToTermsOfService,
+                navigateToHome = navController::navigateToHome,
+            )
+            termsOfServiceScreen(
+                navigateToNickname = navController::navigateToNickname,
+                navigateToLogin = navController::navigateToLogin,
+            )
+            nicknameScreen(
+                navigateToReminder = navController::navigateToTimeReminder,
+                navigateToPrevious = navController::safePopBackStack,
+            )
+            timeReminderScreen(
+                navigateToGuide = navController::navigateToGuide,
+            )
+            guideScreen(
+                navigateToHome = navController::navigateToHome,
+            )
+            homeScreen(
+                navigateToDiaryList = navController::navigateToDiaryList,
+                navigateToSetting = navController::navigateToSetting,
+                navigateToWriteDiary = navController::navigateToWriteDiary,
+                navigateToReplyLoading = navController::navigateToReplyLoading,
+            )
+            diaryListScreen(
+                navigateToHome = navController::navigateToHome,
+                navigateToReplyLoading = navController::navigateToReplyLoading,
+            )
+            writeDiaryScreen(
+                navigateToReplyLoading = navController::navigateToReplyLoading,
+                navigateToHome = navController::navigateToHome,
+                navigateToPrevious = navController::safePopBackStack,
+            )
+            replyLoadingScreen(
+                navigateToReplyDiary = navController::navigateToReplyDiary,
+                navigateToHome = navController::navigateToHome,
+                navigateToDiaryList = navController::navigateToDiaryList,
+            )
+            replyDiaryScreen(
+                navigateToHome = navController::navigateToHome,
+            )
+            settingScreen(
+                navigateToAccountManagement = navController::navigateToAccountManagement,
+                navigateToNotification = navController::navigateToNotificationSetting,
+                navigateToPrevious = navController::safePopBackStack,
+                navigateToWebView = navController::navigateToWebView,
+            )
+            accountManagementScreen(
+                navigateToPrevious = navController::safePopBackStack,
+                navigateToLogin = navController::navigateToLogin,
+            )
+            notificationSettingScreen(
+                navigateToPrevious = navController::safePopBackStack,
+            )
+            webViewScreen(
+                navigateToPrevious = navController::safePopBackStack,
+            )
+        }
+    }
+}
