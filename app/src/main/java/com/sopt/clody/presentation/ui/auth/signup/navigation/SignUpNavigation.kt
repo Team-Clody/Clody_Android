af4805688b1f83e1c106bd03c76b6c78e6e2d3b9
MoonsuKang <rkdms4971@naver.com>
2025-05-23 18:12:12 +0900

    [REFACTOR/#260] SignUpNavigation에서 NavHostController를 NavController로 변경

--- app/src/main/java/com/sopt/clody/presentation/ui/auth/signup/navigation/SignUpNavigation.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/auth/signup/navigation/SignUpNavigation.kt
@@ -2,7 +2,6 @@ package com.sopt.clody.presentation.ui.auth.signup.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.composable
 import com.sopt.clody.presentation.ui.auth.signup.NicknameRoute
@@ -33,7 +32,7 @@ fun NavGraphBuilder.nicknameScreen(
     }
 }
 
-fun NavHostController.navigateToNickname(
+fun NavController.navigateToNickname(
     navOptions: NavOptionsBuilder.() -> Unit = {},
 ) {
     navigate(Route.Nickname, navOptions)
