97859a37a86feaa549becc3d74b1ebc260710fe3
MoonsuKang <rkdms4971@naver.com>
2025-05-27 00:38:23 +0900

    [REFACTOR/#252] 소셜 로그인 로직 core:login 패키지로 분리 및 LoginSdk 추상화
    - KakaoLoginSdk 클래스를 별도로 분리해 login/logout/unlink 책임 위임
    - ViewModel 내 직접 호출하던 카카오 로그인 로직 제거
    - LoginSdk, LoginAccessToken 등 인터페이스 및 value class 도입
    - DI 구조(LoginModule) 정리로 테스트와 확장성 개선

---
+++ app/src/main/java/com/sopt/clody/core/login/KakaoAccessToken.kt
@@ -0,0 +1,4 @@
+package com.sopt.clody.core.login
+
+@JvmInline
+value class KakaoAccessToken(override val value: String) : LoginAccessToken
---
+++ app/src/main/java/com/sopt/clody/core/login/LoginAccessToken.kt
@@ -0,0 +1,5 @@
+package com.sopt.clody.core.login
+
+interface LoginAccessToken {
+    val value: String
+}
