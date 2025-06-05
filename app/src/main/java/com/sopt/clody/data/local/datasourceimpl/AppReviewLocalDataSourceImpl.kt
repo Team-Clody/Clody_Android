d0e33d4db7dbb7135424e76ed609736850739226
SYAAINN <alswo08319@gmail.com>
2025-06-05 15:23:42 +0900

    [FEAT/#273] 리뷰 팝업을 노출할 지에 대한 flag를 담은 SharedPreferences를 분리합니다.

---
+++ app/src/main/java/com/sopt/clody/data/local/datasourceimpl/AppReviewLocalDataSourceImpl.kt
@@ -0,0 +1,19 @@
+package com.sopt.clody.data.local.datasourceimpl
+
+import android.content.SharedPreferences
+import com.sopt.clody.data.local.datasource.AppReviewLocalDataSource
+import com.sopt.clody.di.qualifier.ReviewPrefs
+import javax.inject.Inject
+
+class AppReviewLocalDataSourceImpl @Inject constructor(
+    @ReviewPrefs private val sharedPreferences: SharedPreferences,
+) : AppReviewLocalDataSource {
+
+    override var shouldShowPopup: Boolean
+        get() = sharedPreferences.getBoolean(SHOULD_SHOW_POPUP, true)
+        set(value) = sharedPreferences.edit().putBoolean(SHOULD_SHOW_POPUP, value).apply()
+
+    companion object {
+        private const val SHOULD_SHOW_POPUP = "shouldShowPopup"
+    }
+}
--- app/src/main/java/com/sopt/clody/di/SharedPreferencesModule.kt
+++ app/src/main/java/com/sopt/clody/di/SharedPreferencesModule.kt
@@ -2,6 +2,10 @@ package com.sopt.clody.di
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.sopt.clody.data.local.datasource.AppReviewLocalDataSource
+import com.sopt.clody.data.local.datasourceimpl.AppReviewLocalDataSourceImpl
+import com.sopt.clody.di.qualifier.ReviewPrefs
+import com.sopt.clody.di.qualifier.TokenPrefs
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,7 +18,20 @@ import javax.inject.Singleton
 object SharedPreferencesModule {
     @Provides
     @Singleton
+    @TokenPrefs
     fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
         return context.getSharedPreferences("token_prefs", Context.MODE_PRIVATE)
     }
+
+    @Provides
+    @Singleton
+    @ReviewPrefs
+    fun provideReviewSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences("review_prefs", Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppReviewLocalDataSource(@ReviewPrefs sharedPreferences: SharedPreferences): AppReviewLocalDataSource =
+        AppReviewLocalDataSourceImpl(sharedPreferences)
 }
