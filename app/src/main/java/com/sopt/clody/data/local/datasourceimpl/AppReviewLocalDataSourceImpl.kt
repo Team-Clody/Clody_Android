02cffb491aad022cfef61b323b433dda967b14c6
SYAAINN <alswo08319@gmail.com>
2025-06-06 15:06:16 +0900

    [CHORE/#273] SharedPreferences의 property setter를 개선합니다.

--- app/src/main/java/com/sopt/clody/data/local/datasourceimpl/AppReviewLocalDataSourceImpl.kt
+++ app/src/main/java/com/sopt/clody/data/local/datasourceimpl/AppReviewLocalDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.sopt.clody.data.local.datasourceimpl
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.sopt.clody.data.local.datasource.AppReviewLocalDataSource
 import com.sopt.clody.di.qualifier.ReviewPrefs
 import javax.inject.Inject
@@ -11,7 +12,7 @@ class AppReviewLocalDataSourceImpl @Inject constructor(
 
     override var shouldShowPopup: Boolean
         get() = sharedPreferences.getBoolean(SHOULD_SHOW_POPUP, true)
-        set(value) = sharedPreferences.edit().putBoolean(SHOULD_SHOW_POPUP, value).apply()
+        set(value) = sharedPreferences.edit { putBoolean(SHOULD_SHOW_POPUP, value) }
 
     companion object {
         private const val SHOULD_SHOW_POPUP = "shouldShowPopup"
