79c9a3ea792447a6762e9b05614c06c4918245cb
SYAAINN <alswo08319@gmail.com>
2025-06-06 14:42:15 +0900

    [CHORE/#268] SharedPreferences의 property setter를 개선합니다.

--- app/src/main/java/com/sopt/clody/data/local/datasourceimpl/FirstDraftLocalDataSourceImpl.kt
+++ app/src/main/java/com/sopt/clody/data/local/datasourceimpl/FirstDraftLocalDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.sopt.clody.data.local.datasourceimpl
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.sopt.clody.data.local.datasource.FirstDraftLocalDataSource
 import com.sopt.clody.di.qualifier.FirstDraftPrefs
 import javax.inject.Inject
@@ -10,11 +11,11 @@ class FirstDraftLocalDataSourceImpl @Inject constructor(
 ) : FirstDraftLocalDataSource {
     override var isDraftUsed: Boolean
         get() = sharedPreferences.getBoolean(IS_DRAFT_USED, false)
-        set(value) = sharedPreferences.edit().putBoolean(IS_DRAFT_USED, value).apply()
+        set(value) = sharedPreferences.edit { putBoolean(IS_DRAFT_USED, value) }
 
     override var isFirstUse: Boolean
         get() = sharedPreferences.getBoolean(IS_FIRST_USE, false)
-        set(value) = sharedPreferences.edit().putBoolean(IS_FIRST_USE, value).apply()
+        set(value) = sharedPreferences.edit { putBoolean(IS_FIRST_USE, value) }
 
     companion object {
         private const val IS_DRAFT_USED = "IS_DRAFT_USED"
