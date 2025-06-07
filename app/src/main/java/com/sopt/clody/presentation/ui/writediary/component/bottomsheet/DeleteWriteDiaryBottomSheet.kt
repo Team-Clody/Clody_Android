7a5931bfc5a909bbd7460caa1d6f5d9e41019106
MoonsuKang <rkdms4971@naver.com>
2025-06-07 18:11:32 +0900

    [DEL/#276] 일기삭제 바텀시트 패딩 제거

--- app/src/main/java/com/sopt/clody/presentation/ui/writediary/component/bottomsheet/DeleteWriteDiaryBottomSheet.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/writediary/component/bottomsheet/DeleteWriteDiaryBottomSheet.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -67,7 +66,6 @@ fun DeleteWriteDiaryBottomSheet(
                     )
                 }
                 Spacer(modifier = Modifier.navigationBarsPadding())
-                Spacer(modifier = Modifier.height(60.dp))
             }
         },
     )
