cab42dba15324d269453f77faadadd0381e57533
MoonsuKang <rkdms4971@naver.com>
2025-06-07 17:19:49 +0900

    [REFACTOR/#270] 클로버 아이콘 타입 분기 로직 개선 및 임시저장 상태 대응 추가

--- app/src/main/java/com/sopt/clody/domain/model/ReplyStatus.kt
+++ app/src/main/java/com/sopt/clody/domain/model/ReplyStatus.kt
@@ -4,5 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 enum class ReplyStatus {
-    UNREADY, READY_READ, READY_NOT_READ
+    UNREADY, READY_READ, READY_NOT_READ, HAS_DRAFT, INVALID_DRAFT;
+
+    val isUnreadOrNotRead: Boolean
+        get() = this == UNREADY || this == READY_NOT_READ
 }
