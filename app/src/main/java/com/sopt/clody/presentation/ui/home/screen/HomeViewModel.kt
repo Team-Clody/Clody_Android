cb11c8eb1186f5c664c0b362b13d2439b220d3f5
MoonsuKang <rkdms4971@naver.com>
2025-06-17 01:50:14 +0900

    [MOD/#280] 사용하지 않는 상태 제거

--- app/src/main/java/com/sopt/clody/presentation/ui/home/screen/HomeViewModel.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/screen/HomeViewModel.kt
@@ -150,7 +150,6 @@ class HomeViewModel @Inject constructor(
                     _hasDraft.value = dailyResponse.isDraft
                     _diaryCount.value = dailyResponse.diaries.size
                     _isDeleted.value = dailyResponse.isDeleted
-                    _replyStatus.value = ReplyStatus.UNREADY
 
                     setErrorState(false)
                     DailyDiariesState.Success(dailyResponse)
