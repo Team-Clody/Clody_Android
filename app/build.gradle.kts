864dcbda5e957bcaa4bfab5e30d7ebe87141f89f
MoonsuKang <rkdms4971@naver.com>
2025-07-15 00:06:00 +0900

    [MOD/#300] 버전 정보 업데이트
    versionCode 27 -> 28
    versionName 1.3.0 -> 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.sopt.clody"
         minSdk = 28
         targetSdk = 35
-        versionCode = 27
-        versionName = "1.3.0"
+        versionCode = 28
+        versionName = "1.4.0"
         val kakaoApiKey: String = properties.getProperty("kakao.api.key")
         val amplitudeApiKey: String = properties.getProperty("amplitude.api.key")
         val googleAdmobAppId: String = properties.getProperty("GOOGLE_ADMOB_APP_ID", "")
