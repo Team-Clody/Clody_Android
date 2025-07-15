eb7a018276f28875da1129adbf52233330bbdcc8
SYAAINN <alswo08319@gmail.com>
2025-07-15 10:37:19 +0900

    [CHORE/#293] KtlintFormat을 적용합니다.

--- app/src/main/java/com/sopt/clody/presentation/ui/auth/component/button/KaKaoButton.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/auth/component/button/KaKaoButton.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -33,7 +31,7 @@ fun KaKaoButton(
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(containerColor = ClodyTheme.colors.kakaoYellow),
         shape = RoundedCornerShape(10.dp),
-        modifier = modifier.height(48.dp)
+        modifier = modifier.height(48.dp),
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
