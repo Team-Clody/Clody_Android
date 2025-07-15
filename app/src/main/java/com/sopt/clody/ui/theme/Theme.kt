d1a17aaa3ecf7412e574e54d03e1aee0ec1737d8
SYAAINN <alswo08319@gmail.com>
2025-07-15 13:30:30 +0900

    [FEAT/#299] 한국어 typo, 영어 typo를 정의합니다. - lineHeight 계산 확장함수 생성 - 네이밍 변경

--- app/src/main/java/com/sopt/clody/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/clody/ui/theme/Theme.kt
@@ -3,12 +3,26 @@ package com.sopt.clody.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.platform.LocalConfiguration
+
+@Composable
+fun provideTypographyByLocale(): ClodyTypography {
+    val locale = LocalConfiguration.current.locales[0]
+    return if (locale.language == "ko") clodyKoreanTypography else clodyEnglishTypography
+}
 
 @Composable
 fun ClodyTheme(
     content: @Composable () -> Unit,
 ) {
-    CompositionLocalProvider(content = content)
+    val colors = defaultClodyColors
+    val typography = provideTypographyByLocale()
+
+    CompositionLocalProvider(
+        LocalClodyColors provides colors,
+        LocalClodyTypography provides typography,
+        content = content,
+    )
 }
 
 object ClodyTheme {
