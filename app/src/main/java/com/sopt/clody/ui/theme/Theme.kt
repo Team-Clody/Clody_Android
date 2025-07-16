c28221c68f15c2832a2b3dee2ec924cb465c28a9
SYAAINN <alswo08319@gmail.com>
2025-07-16 13:07:03 +0900

    [CHORE/#299] 코드리뷰 사항을 반영합니다.

--- app/src/main/java/com/sopt/clody/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/clody/ui/theme/Theme.kt
@@ -3,11 +3,14 @@ package com.sopt.clody.ui.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalConfiguration
 
 @Composable
 fun provideTypographyByLocale(): ClodyTypography {
-    val locale = LocalConfiguration.current.locales[0]
+    val configuration = LocalConfiguration.current
+    val locale = remember(configuration) { configuration.locales[0] }
+
     return if (locale.language == "ko") clodyKoreanTypography else clodyEnglishTypography
 }
 
