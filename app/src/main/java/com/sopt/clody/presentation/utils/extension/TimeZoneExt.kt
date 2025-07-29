8f0e438e858fbff6a762ad9a593725e872f87cd2
SYAAINN <alswo08319@gmail.com>
2025-07-29 21:52:31 +0900

    [CHORE/#309] 코드레빗 리뷰사항을 반영합니다. - referenceDate 주입으로 안정성 증가 (동일 기준일자 보장, 날짜 경계 오류 방, 디버깅 및 테스트 용이) - hour, minute 을 Int -> String 으로 타입 및 포맷 변경

--- app/src/main/java/com/sopt/clody/presentation/utils/extension/TimeZoneExt.kt
+++ app/src/main/java/com/sopt/clody/presentation/utils/extension/TimeZoneExt.kt
@@ -9,18 +9,19 @@ import java.time.format.DateTimeFormatter
 /**
 *   @param time 서버로부터 수신받은 시간으로 "21:30" 와 같은 형태로 전달받는다.
 * */
-fun convertKSTtoUTZ(time: String): Triple<TimePeriod, Int, Int> {
+fun convertKSTtoUTZ(time: String, referenceDate: LocalDate = LocalDate.now()): Triple<TimePeriod, String, String> {
     val kstZoneId = ZoneId.of("Asia/Seoul")
     val userZoneId = ZoneId.systemDefault()
 
     val formatter = DateTimeFormatter.ofPattern("HH:mm")
     val localTime = LocalTime.parse(time, formatter)
 
-    val kstDateTime = ZonedDateTime.of(LocalDate.now(kstZoneId), localTime, kstZoneId)
+    val kstDateTime = ZonedDateTime.of(referenceDate, localTime, kstZoneId)
     val userDateTime = kstDateTime.withZoneSameInstant(userZoneId)
 
     val hour24 = userDateTime.hour
     val minute = userDateTime.minute
+
     val timePeriod = if (hour24 < 12) TimePeriod.AM else TimePeriod.PM
     val hour12 = when {
         hour24 == 0 -> 12
@@ -28,7 +29,10 @@ fun convertKSTtoUTZ(time: String): Triple<TimePeriod, Int, Int> {
         else -> hour24
     }
 
-    return Triple(timePeriod, hour12, minute)
+    val hourFormatted = hour12.toString()
+    val minuteFormatted = String.format("%02d", minute)
+
+    return Triple(timePeriod, hourFormatted, minuteFormatted)
 }
 
 /**
@@ -36,21 +40,21 @@ fun convertKSTtoUTZ(time: String): Triple<TimePeriod, Int, Int> {
  *  @param hour 시간
  *  @param minute 분
  * */
-fun convertUTZtoKST(timePeriod: TimePeriod, hour: String, minute: String): String {
+fun convertUTZtoKST(timePeriod: TimePeriod, hour: String, minute: String, referenceDate: LocalDate = LocalDate.now()): String {
     val userZoneId = ZoneId.systemDefault()
     val kstZoneId = ZoneId.of("Asia/Seoul")
 
     val hour24 = when (timePeriod) {
         TimePeriod.AM -> if (hour == "12") 0 else hour.toInt()
-        TimePeriod.PM -> if (hour == "12") 12 else (hour + 12).toInt()
+        TimePeriod.PM -> if (hour == "12") 12 else hour.toInt() + 12
     }
 
     val userTime = LocalTime.of(hour24, minute.toInt())
-    val userZoned = ZonedDateTime.of(LocalDate.now(userZoneId), userTime, userZoneId)
+    val userZoned = ZonedDateTime.of(referenceDate, userTime, userZoneId)
     val kstZoned = userZoned.withZoneSameInstant(kstZoneId)
 
     val kstHour = kstZoned.hour
     val kstMinute = kstZoned.minute
 
-    return String.format("%02s:%02s", kstHour, kstMinute)
+    return String.format(java.util.Locale.ROOT, "%02d:%02d", kstHour, kstMinute)
 }
