f3764ffa253817e326dfe8653117fb541279de70
SYAAINN <alswo08319@gmail.com>
2025-08-02 12:05:07 +0900

    [CHORE/#314] 알림 시간 설정 완료 토스트 메시지의 위치를 수정합니다.

--- app/src/main/java/com/sopt/clody/presentation/ui/setting/notificationsetting/screen/NotificationSettingScreen.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/setting/notificationsetting/screen/NotificationSettingScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -92,6 +93,7 @@ fun NotificationSettingRoute(
                 contentColor = ClodyTheme.colors.white,
                 durationMillis = 3000,
                 onDismiss = { notificationSettingViewModel.resetNotificationTimeChangeState() },
+                modifier = Modifier.navigationBarsPadding(),
             )
         }
     }
