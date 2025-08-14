89f56241bb8e2f9615a2a5b2dad0f067c3af0167
SYAAINN <alswo08319@gmail.com>
2025-08-14 21:20:01 +0900

    [CHORE/#317] ktlintFormat

--- app/src/main/java/com/sopt/clody/presentation/ui/home/screen/HomeViewModel.kt
+++ app/src/main/java/com/sopt/clody/presentation/ui/home/screen/HomeViewModel.kt
@@ -2,7 +2,6 @@ package com.sopt.clody.presentation.ui.home.screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sopt.clody.ClodyFirebaseMessagingService.Companion.getTokenFromPreferences
 import com.sopt.clody.core.fcm.FcmTokenProvider
 import com.sopt.clody.core.network.NetworkConnectivityObserver
 import com.sopt.clody.core.network.NetworkStatus
@@ -15,7 +14,6 @@ import com.sopt.clody.domain.repository.DiaryRepository
 import com.sopt.clody.domain.repository.DraftRepository
 import com.sopt.clody.domain.repository.NotificationRepository
 import com.sopt.clody.domain.repository.ReviewRepository
-import com.sopt.clody.presentation.ui.auth.timereminder.TimeReminderState
 import com.sopt.clody.presentation.ui.home.calendar.model.DiaryDateData
 import com.sopt.clody.presentation.ui.setting.notificationsetting.screen.NotificationChangeState
 import com.sopt.clody.presentation.utils.network.ErrorMessageProvider
